72916253263677f7fd94cf76891daae918996f43
Sven Fuchs <svenfuchs@artweb-design.de>
2008-06-11 17:52:53 +0200

    fix for Rails 2.1 dirty tracking

--- vendor/plugins/has_options/lib/has_options.rb
+++ vendor/plugins/has_options/lib/has_options.rb
@@ -1,33 +1,34 @@
+# TODO allow a :type option and typecast the value
+
 module HasOptions  
   class << self
     def included(base)
       base.class_eval do
         extend ClassMethods
         class_inheritable_accessor :option_definitions
+        self.option_definitions = {}
         serialize :options        
       end
     end
     
-    def option(*args)
-      include HasOptions unless included_modules.include? HasOptions
-      option *args
-    end
-  end
-  
-  module ClassMethods    
-    def option(name, definition = {})
-      self.option_definitions ||= {}
-      self.option_definitions[name] = definition.reverse_update(:default => nil, :type => :text_field)
-      class_eval %Q(
-        def #{name}
-          self.options ||= {}
-          options[:#{name}] || option_definitions[:#{name}][:default]
-        end                
-        def #{name}=(value)
-          self.options ||= {}
-          options[:#{name}] = value
-        end
-      ), __FILE__, __LINE__
+    module ClassMethods    
+      def has_option(name, definition = {})
+        self.option_definitions[name] = definition.reverse_update(:default => nil, :type => :text_field)
+        class_eval %Q(
+          def #{name}
+            #{name}_before_type_cast
+          end
+          def #{name}_before_type_cast
+            self.options ||= {}
+            options[:#{name}] || option_definitions[:#{name}][:default]
+          end                
+          def #{name}=(value)
+            options_will_change!
+            self.options ||= {}
+            options[:#{name}] = value
+          end
+        ), __FILE__, __LINE__
+      end
     end
   end
 end
