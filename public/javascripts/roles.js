6e8cede3dbe357c2fa7a605718c3fe51d7ead882
Sven Fuchs <svenfuchs@artweb-design.de>
2008-06-27 16:50:34 +0200

    always reveal 'visible-for anonymous' elements

--- public/javascripts/roles.js
+++ public/javascripts/roles.js
@@ -2,9 +2,15 @@ function authorize_elements(roles) {
 	if(roles.include('superuser')) {
 		var elements = $$(".visible-for");
 	} else {
-		var elements = roles.map(function(role){ $$("." + role) }).flatten();
+		var elements = roles.map(function(role){ return $$("." + role) }).flatten();
 	}
   elements.each(function(element) {
-    element.removeClassName('visible-for');
+		if(element) {
+    	element.removeClassName('visible-for');
+		}
   });
 }
+
+Event.onReady(function() {
+	authorize_elements(new Array('anonymous'))
+});
