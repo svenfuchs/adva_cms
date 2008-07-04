7be913af5a2f20625c8f4c2a117adec0e1627b6b
Sven Fuchs <svenfuchs@artweb-design.de>
2008-07-05 01:48:29 +0200

    make index and show action explicit on admin/users_controller

--- vendor/engines/adva_user/app/controllers/admin/users_controller.rb
+++ vendor/engines/adva_user/app/controllers/admin/users_controller.rb
@@ -7,6 +7,12 @@ class Admin::UsersController < Admin::BaseController
   
   helper_method :collection_path, :member_path, :new_member_path, :edit_member_path
   
+  def index
+  end
+  
+  def show
+  end
+  
   def new
     @user = User.new
   end
@@ -22,6 +28,9 @@ class Admin::UsersController < Admin::BaseController
       flash.now[:error] = "The user account could not be created."
       render :action => :new
     end
+  end
+  
+  def edit
   end
   
   def update
