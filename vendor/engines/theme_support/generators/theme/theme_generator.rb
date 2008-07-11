94008f82ac1bc8c1088af98e7be29e9ac56ef960
TomK32 <tomk32@tomk32.de>
2008-07-12 00:37:51 +0200

    renamend and extended views_readme added two example views fixed a few paths renamed layout for rails2 changed theme.yml to about.yml

--- vendor/engines/theme_support/generators/theme/theme_generator.rb
+++ vendor/engines/theme_support/generators/theme/theme_generator.rb
@@ -6,21 +6,27 @@ class ThemeGenerator < Rails::Generator::NamedBase
           # theme content folders
           m.directory File.join( "themes", file_name, "images" )
           m.directory File.join( "themes", file_name, "javascript" )
-          m.directory File.join( "themes", file_name, "layouts" )
-          m.directory File.join( "themes", file_name, "views" )
+          m.directory File.join( "themes", file_name, "templates" )
+          m.directory File.join( "themes", file_name, "templates", "layouts" )
+          m.directory File.join( "themes", file_name, "templates", "blog" )
+          m.directory File.join( "themes", file_name, "templates", "shared" )
           m.directory File.join( "themes", file_name, "stylesheets" )
           # Default files...
           # about
           m.template 'about.markdown', File.join( 'themes', file_name, 'about.markdown' )
+          m.template 'about.yml', File.join( 'themes', file_name, 'about.yml' )
           # image
           m.file 'preview.png', File.join( 'themes', file_name, 'images', 'preview.png' )
           # stylesheet
           m.template "theme.css", File.join( "themes", file_name, "stylesheets", "#{file_name}.css" )
+          # views
+          m.template 'blog_index.html.erb', File.join( 'themes', file_name, 'templates', 'blog', 'index.html.erb' )
+          m.template '_footer.html.erb', File.join( 'themes', file_name, 'templates', 'shared', '_footer.html.erb' )
           # layouts
-          m.template 'layout.rhtml', File.join( 'themes', file_name, 'layouts', 'default.rhtml' )
-          #m.template 'layout.liquid', File.join( 'themes', file_name, 'layouts', 'default.liquid' )
-          # view readme
-          m.template 'views_readme', File.join( 'themes', file_name, 'views', 'views_readme.txt' )
+          m.template 'layout.erb.html', File.join( 'themes', file_name, 'templates', 'layouts', 'default.html.erb' )
+          # readme
+          m.template 'README', File.join( 'themes', file_name, 'README' )
+          m.readme 'README'
       end
    end
 end
