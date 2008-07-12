81cbf020f5c3d4ccde815d2706d52f682042def3
TomK32 <tomk32@tomk32.de>
2008-07-12 12:23:32 +0200

    added routes like in mephisto to send theme images, js etc.

--- vendor/engines/theme_support/routes.rb
+++ vendor/engines/theme_support/routes.rb
@@ -13,3 +13,9 @@ map.connect ":type/themes/:subdir/:theme_id/*file",
 # map.connect ":type/themes/:theme_id/:file", options
 
 
+map.with_options :controller => 'theme', :action => 'file' do |m| 
+  m.connect ':type/:theme_id/*file',       :type => /stylesheets|javascripts|images/
+  m.css    'stylesheets/:theme_id/:path.:ext', :type => 'stylesheets'
+  m.js     'javascripts/:theme_id/:path.:ext', :type => 'javascripts'
+  m.images 'images/:theme_id/:path.:ext',      :type => 'images'
+end
