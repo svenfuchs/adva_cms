510a3a9b037933c840ea7f6d641d617222542732
benedikt <benedikt@synatic.net>
2008-08-22 09:23:43 +0200

    fixed admin_site_select_tag to display current site and moved some logic from the view into the helper

--- vendor/engines/adva_cms/app/helpers/admin/base_helper.rb
+++ vendor/engines/adva_cms/app/helpers/admin/base_helper.rb
@@ -1,19 +1,22 @@
 # TODO move this to the base_helper?
 
 module Admin::BaseHelper
-  def admin_site_select_tag(path, user)
-    # TODO only show sites where the user actually has access to!
-    options  = []
-    options += Site.find(:all).collect { |site| [site.name, admin_site_path(site)] }
-    select_tag 'site-select', options_for_select(options, path)
-  end
-
-  def admin_global_select_tag(path)
+  def admin_site_select_tag
+    return unless current_user.has_role?(:superuser) || Site.multi_sites_enabled
     options = []
-    options  = [['Sites overview', admin_sites_path]]
-    options << ['Superusers + Admins', admin_users_path]
-    options << ['------------------', '#']
-    options += Site.find(:all).collect { |site| [site.name, admin_site_path(site)] }
-    select_tag 'site-select', options_for_select(options, path)
+
+    if current_user.has_role?(:superuser)
+      options << ['Sites overview', admin_sites_path]
+      options << ['Superusers + Admins', admin_users_path]
+      options << ['------------------', '#']
+    end
+
+
+    # TODO only show sites where the user actually has access to!
+    options += Site.all.collect { |site| [site.name, admin_site_path(site)] }
+
+    selection = options.reverse.detect { |name, url| request.path.starts_with?(url) }
+
+    select_tag 'site-select', options_for_select(options, selection)
   end
 end
