53fa376d80f45c175f32964c6a7b25091d590064
Sven Fuchs <svenfuchs@artweb-design.de>
2008-09-15 16:53:49 +0200

    fix base_helper_spec

--- spec/helpers/admin/base_helper_spec.rb
+++ spec/helpers/admin/base_helper_spec.rb
@@ -6,6 +6,12 @@ describe Admin::BaseHelper do
   before :each do
     @user = stub_user
     helper.stub!(:current_user).and_return @user
+    
+    helper.stub!(:admin_sites_path).and_return 'admin_sites_path'
+    helper.stub!(:admin_users_path).and_return 'admin_users_path'
+    helper.stub!(:admin_site_path).and_return 'admin_site_path'
+    
+    helper.stub!(:request).and_return mock('request', :path => 'path')
   end
 
   describe "#admin_site_select_tag" do
@@ -16,11 +22,11 @@ describe Admin::BaseHelper do
       end
 
       it "shows the site overview option in the site select menu" do
-        helper.admin_site_select_tag.should have_tag('select#site-select option[value=?]', admin_sites_path)
+        helper.admin_site_select_tag.should have_tag('select#site-select option[value=?]', 'admin_sites_path')
       end
 
       it "shows the user manager option in the site select menu" do
-        helper.admin_site_select_tag.should have_tag('select#site-select option[value=?]', admin_users_path)
+        helper.admin_site_select_tag.should have_tag('select#site-select option[value=?]', 'admin_users_path')
       end
     end
 
@@ -28,13 +34,13 @@ describe Admin::BaseHelper do
       before :each do
         @user.stub!(:has_role?).with(:superuser).and_return false
       end
-
+    
       it "shows the site overview option in the site select menu" do
-        helper.admin_site_select_tag.should_not have_tag('select#site-select option[value=?]', admin_sites_path)
+        helper.admin_site_select_tag.should_not have_tag('select#site-select option[value=?]', 'admin_sites_path')
       end
-
+    
       it "shows the user manager option in the site select menu" do
-        helper.admin_site_select_tag.should_not have_tag('select#site-select option[value=?]', admin_users_path)
+        helper.admin_site_select_tag.should_not have_tag('select#site-select option[value=?]', 'admin_users_path')
       end
     end
   end
