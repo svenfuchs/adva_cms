29551cc1760f2e653d02aa9c7375c30487ee3c3d
Josh Harvey <joshmh@gmail.com>
2008-09-23 21:11:34 +0300

    cleaned up long line

--- vendor/engines/adva_blog/app/controllers/blog_controller.rb
+++ vendor/engines/adva_blog/app/controllers/blog_controller.rb
@@ -6,7 +6,8 @@ class BlogController < BaseController
   before_filter :set_article, :only => :show
   before_filter :guard_view_permissions, :only => :show
 
-  caches_page_with_references :index, :show, :track => ['@article', '@articles', '@category', {'@site' => :tag_counts, '@section' => :tag_counts}]
+  caches_page_with_references :index, :show, 
+    :track => ['@article', '@articles', '@category', {'@site' => :tag_counts, '@section' => :tag_counts}]
   authenticates_anonymous_user
   acts_as_commentable
 
