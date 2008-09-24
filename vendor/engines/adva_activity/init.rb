3cd76b99da9d59493aa175d339c8d1b5b42a2e00
Clemens Kofler <clemens@railway.at>
2008-09-24 17:22:36 +0200

    Moved activity hook to init hook.

--- vendor/engines/adva_activity/init.rb
+++ vendor/engines/adva_activity/init.rb
@@ -1,6 +1,13 @@
 # remove plugin from load_once_paths 
 Dependencies.load_once_paths -= Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
 
-require 'activities/site'
+# hook activities into site
+module Activities
+  def self.included(base)
+    base.has_many :activities, :dependent => :destroy
+  end
+end
+
+Activities.include_into 'Site'
 
 ActiveRecord::Base.observers += %w(activities/article_observer activities/comment_observer activities/wikipage_observer)
