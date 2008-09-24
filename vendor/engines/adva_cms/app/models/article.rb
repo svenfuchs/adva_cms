cafb2aa5934ba632ad37b59949e84d5ce43f4dec
Clemens Kofler <clemens@railway.at>
2008-09-24 12:17:39 +0200

    DRYed up Article#full_permalink.

--- vendor/engines/adva_cms/app/models/article.rb
+++ vendor/engines/adva_cms/app/models/article.rb
@@ -25,11 +25,8 @@ class Article < Content
   def full_permalink
     raise "can not create full_permalink for an article that belongs to a non-blog section" unless section.is_a? Blog
     # raise "can not create full_permalink for an unpublished article" unless published?
-    if published?
-      Hash[:permalink, permalink, *[:year, :month, :day].map {|key| [key, published_at.send(key)] }.flatten]
-    else
-      Hash[:permalink, permalink, *[:year, :month, :day].map {|key| [key, created_at.send(key)] }.flatten]
-    end
+    date = [:year, :month, :day].map { |key| [key, (published? ? published_at : created_at).send(key)] }.flatten
+    Hash[:permalink, permalink, *date]
   end
 
   def primary?
