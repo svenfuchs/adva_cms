6a4291d7db3dbd1e3bfd477fa3bc83adbf83516c
Marko Seppae <marko.seppa@gmail.com>
2008-10-28 10:34:57 +0100

    moved some files

--- test/factories/categories.rb
+++
@@ -1,7 +0,0 @@
-Factory.define :category do |c|
-  c.title "General Information"
-end
-
-Factory.define :unrelated_category, :class => Category do |c|
-  c.title "Private Rantings"
-end
--- test/factories/tag.rb
+++
@@ -1,7 +0,0 @@
-Factory.define :tag do |t|
-  t.name "rails"
-end
-
-Factory.define :unrelated_tag, :class => Tag do |t|
-  t.name "java"
-end
