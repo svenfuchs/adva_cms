62a4282c0575aae2451de293d71f29609cfe0857
Marko Seppae <marko.seppa@gmail.com>
2008-10-30 10:43:30 +0100

    removed unnecessary files

--- config/initializers/decorations.rb
+++
@@ -1,9 +0,0 @@
-# this file includes embellishments to the engine models needed for the SaaS app
-
-# add association to Site
-require 'Site'
-
-class Site < ActiveRecord::Base
-  has_one :account_site_mapping
-  has_one :account, :through => :account_site_mapping 
-end
