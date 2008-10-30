119d80cb71e988e009538f69234aa52e7a9023b0
Sven Fuchs <svenfuchs@artweb-design.de>
2008-10-30 10:52:30 +0100

    fix user factory for stories

--- stories/factories/user.rb
+++ stories/factories/user.rb
@@ -4,15 +4,8 @@ factory :user,
         :name => 'name',
         :email => 'email@email.org',
         :login => 'login',
-        :password => 'password',
-        :password_confirmation => 'password'
+        :password => 'password'
 
 factory :author, valid_user_attributes, :class => :user
 
 factory :verified_user, valid_user_attributes.update(:verified_at => '2008-01-01 12:00:00'), :class => :user
-
-Anonymous.delete_all
-
-# factory :anonymous,
-#         :name => 'anonymous',
-#         :email => 'anonymous@email.org'
