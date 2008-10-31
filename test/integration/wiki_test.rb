fe5ed53f697ccf0e1eaa12680357c0ee5013a249
Marko Seppae <marko.seppa@gmail.com>
2008-10-31 11:41:00 +0100

    converted whole viewing_a_wikipage story to integration test

--- test/integration/wiki_test.rb
+++ test/integration/wiki_test.rb
@@ -106,6 +106,9 @@ class WikiTest < ActionController::IntegrationTest
     # go to wikipage show page
     get "/pages/wiki-home"
     
+    # the page renders the wiki index page
+    assert_template 'wiki/show', "Wiki should have a template for show view"
+    
     # authorized link should be visible only for following people:
     user            = "user-#{wikipage.author.id}"
     site_admin      = "site-#{site.id}-admin"
@@ -114,9 +117,6 @@ class WikiTest < ActionController::IntegrationTest
     wikipage_owner  = "content-#{wikipage.author.id}-owner"
     visible_for = "user #{user} #{wikipage_author} #{wiki_moderator} #{site_admin} #{wikipage_owner} superuser"
     
-    # the page renders the wikipage show page
-    assert_template 'wiki/show', "Wikipage should have a template for show view"
-    
     # the page should show the wikipage ...
     assert_select "div#wikipage_#{wikipage.id}", true, "The page should containt the wikipage" do
       # ... with meta info of wikipage ...
@@ -124,7 +124,7 @@ class WikiTest < ActionController::IntegrationTest
         # ... with link to wikipages index ...
         assert_select "a[href$='pages']", true, "The page should contain a link to wikipages index."
         # ... with revision number ...
-        assert_select "h4", /revision: /, "The meta information of the wikipage should contain a revision number."
+        assert_select "h4", /revision: #{wikipage.version}/, "The meta information of the wikipage should contain a revision number."
         # ... with author name ...
         assert_select "p", /by: John Doe/, "The meta information of the wikipage should contain the name of the author"
       end
@@ -144,7 +144,7 @@ class WikiTest < ActionController::IntegrationTest
             assert_select "a[href$='edit']", true, "The page should contain the edit link."
           end
           # ... with link to go to previous version ...
-          assert_select "a[href$='rev/1']", true, "The page should contain the previous revision link."
+          assert_select "a[href$='rev/#{wikipage.version - 1}']", true, "The page should contain the previous revision link."
         end
       end
     end
@@ -154,4 +154,41 @@ class WikiTest < ActionController::IntegrationTest
   end
   
   # Scenario: Viewing a wikipage revision
+  def test_viewing_wikipages_revision
+    site = Factory :site_with_wiki
+    wiki = site.sections.first
+    wiki.wikipages << Factory(:wikipage)
+    wikipage = wiki.wikipages.first
+    
+    # create a new version
+    wikipage.update_attributes! :body => 'Updated wikipage body.'
+    
+    # go to wikipage show page
+    get "/pages/wiki-home"
+    
+    assert_select "div.content" do
+      # the page should display updated wikipage body
+      assert_select "p", /Updated wikipage body./, "The content of the wikipage should contain the body of the updated wikipage."
+    end
+    
+    # The page should have a link to go to previous version ...
+    assert_select "a[href$='rev/#{wikipage.version - 1}']", true, "The page should contain the previous revision link."
+    
+    # go back one revision
+    get wikipage_rev_path(:section_id => wiki.id, :id => wikipage.permalink, :version => (wikipage.version - 1))
+    
+    assert_select "div.content" do
+      # the page should display updated wikipage body
+      assert_select "p", /this is a wiki home page/, "The content of the wikipage should contain the previous body of the wikipage."
+    end
+    
+    # The page should have a link return to current version
+    assert_select "a[href$='#{wikipage.permalink}']", true, "The page should contain the link for returning to present version"
+    
+    # The page should have a link to go to previous version
+    assert_select "a[href$='#{wikipage.permalink}?version=#{wikipage.version - 1}']", true, "The page should contain the link to rollback."
+    
+    # check that the index page is cached
+    assert_page_cached
+  end
 end
