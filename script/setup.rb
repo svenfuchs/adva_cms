575004f7eb5908c26d84fd25c988b937f9c27060
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-02 19:15:01 +0100

    add script/setup.rb for patching environment.rb and routes.rb

---
+++ script/setup.rb
@@ -0,0 +1,15 @@
+config_path = File.expand_path(File.dirname(__FILE__) + '/../../..//config')
+
+def patch_file(path, after, insert)
+  content = File.open(path){|f| f.read }
+  content.gsub!(after, "#{after}\n#{insert}") unless content =~ /#{insert}/
+  File.open(path, 'w'){|f| f.write(content) }
+end
+
+patch_file config_path + '/environment.rb',
+           "require File.join(File.dirname(__FILE__), 'boot')",
+           "require File.join(File.dirname(__FILE__), '../vendor/adva/engines/adva_cms/boot')"
+
+patch_file config_path + '/routes.rb',
+           "ActionController::Routing::Routes.draw do |map|",
+           "\tAdvaCms::Routes.from_plugins(map)\n"
