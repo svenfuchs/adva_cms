e7dcb64602a507f7beb62097785ba47904843c16
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-02 21:49:39 +0100

    also delete the public/index.html in setup.rb

--- script/setup.rb
+++ script/setup.rb
@@ -2,9 +2,11 @@ rails_root = File.expand_path(File.dirname(__FILE__) + '/../../..')
 
 def patch_file(path, after, insert)
   content = File.open(path){|f| f.read }
-  content.gsub!(after, "#{after}\n#{insert}") unless content =~ /#{insert}/
+  content.gsub!(after, "#{after}\n#{insert}") unless content =~ /#{insert}/mi
   File.open(path, 'w'){|f| f.write(content) }
 end
+
+File.unlink rails_root + '/public/index.html'
 
 patch_file rails_root + '/config/environment.rb',
            "require File.join(File.dirname(__FILE__), 'boot')",
@@ -17,4 +19,3 @@ patch_file rails_root + '/config/routes.rb',
 patch_file rails_root + '/Rakefile',
            "require 'tasks/rails'",
            "\nload 'vendor/adva/plugins/engines/tasks/engines.rake'"
-           
