1e8e95790409d250df15c3177f462b7ad19e5f8c
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-02 20:02:33 +0100

    add patching rakefile to setup.rb and update README

--- script/setup.rb
+++ script/setup.rb
@@ -1,4 +1,4 @@
-config_path = File.expand_path(File.dirname(__FILE__) + '/../../..//config')
+rails_root = File.expand_path(File.dirname(__FILE__) + '/../../..')
 
 def patch_file(path, after, insert)
   content = File.open(path){|f| f.read }
@@ -6,10 +6,15 @@ def patch_file(path, after, insert)
   File.open(path, 'w'){|f| f.write(content) }
 end
 
-patch_file config_path + '/environment.rb',
+patch_file rails_root + '/config/environment.rb',
            "require File.join(File.dirname(__FILE__), 'boot')",
            "require File.join(File.dirname(__FILE__), '../vendor/adva/engines/adva_cms/boot')"
 
-patch_file config_path + '/routes.rb',
+patch_file rails_root + '/config/routes.rb',
            "ActionController::Routing::Routes.draw do |map|",
            "\tmap.from_plugins\n"
+           
+patch_file rails_root + '/Rakefile',
+           "require 'tasks/rails'",
+           "\nload 'vendor/adva/plugins/engines/tasks/engines.rake'"
+           
