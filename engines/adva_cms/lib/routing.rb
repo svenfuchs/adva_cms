e9d9730226ea4ae4783e1728c8445c241c10efb5
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-02 18:56:16 +0100

    move docs and add routing_filter calls to adva_cms

--- engines/adva_cms/lib/routing.rb
+++ engines/adva_cms/lib/routing.rb
@@ -5,6 +5,12 @@ module AdvaCms
 
     # Loads routing for engines starting with 'adva_'
     def self.from_plugins(map)
+      map.filter 'locale'
+      map.filter 'categories' # TODO fix: around_filter seems to call filters in reverse order
+      map.filter 'section_root'
+      map.filter 'section_paths'
+      map.filter 'pagination'
+
       engines  = Engines.plugins.collect(&:name).select { |name| name =~ /^adva_/ }
       engines += ADDITIONAL_ENGINES
       engines.each do |engine|
