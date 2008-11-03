441bbba828683a4358f0e24a62f5e88197ab4fb5
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-03 14:53:27 +0100

    picked josh's change to session controller: skip some before_filters

--- engines/adva_user/app/controllers/session_controller.rb
+++ engines/adva_user/app/controllers/session_controller.rb
@@ -1,4 +1,6 @@
 class SessionController < BaseController
+  skip_before_filter :set_site, :set_section, :set_cache_root
+  
   authentication_required :except => [:new, :create]
   renders_with_error_proc :below_field
 
