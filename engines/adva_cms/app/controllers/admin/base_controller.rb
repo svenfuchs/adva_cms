145a7ca0d4acfb7c2cbb40a42cd90b9ebefd4d7a
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-03 19:48:36 +0100

    use has_permission? instead of has_role? on user/edit

--- engines/adva_cms/app/controllers/admin/base_controller.rb
+++ engines/adva_cms/app/controllers/admin/base_controller.rb
@@ -8,6 +8,7 @@ class Admin::BaseController < ApplicationController
   before_filter :set_site, :set_locale, :set_timezone, :set_cache_root
   helper :base, :content, :comments, :users
   helper_method :admin_section_contents_path, :new_admin_content_path, :perma_host
+  helper_method :has_permission?
 
   authentication_required
 
--- engines/adva_rbac/spec/spec_roles_helper.rb
+++ engines/adva_rbac/spec/spec_roles_helper.rb
@@ -1,39 +1,39 @@
 module SpecRolesHelper
   def define_roles!
-    Rbac::Role.define :anonymous, 
+    Rbac::Role.define :anonymous,
                       :grant => true
-                      
-    Rbac::Role.define :user, 
-                      :grant => :registered?, 
+
+    Rbac::Role.define :user,
+                      :grant => :registered?,
                       :parent => :anonymous,
                       :message => 'You need to be logged in to perform this action.'
 
-    Rbac::Role.define :author, 
-                      :require_context => Comment, 
-                      :grant => lambda{|context, user| context && !!context.try(:is_author?, user) }, 
+    Rbac::Role.define :author,
+                      :require_context => Comment,
+                      :grant => lambda{|context, user| context && !!context.try(:is_author?, user) },
                       :parent => :user,
                       :message => 'You need to be the author of this object to perform this action.'
 
-    Rbac::Role.define :moderator, 
-                      :require_context => Section, 
+    Rbac::Role.define :moderator,
+                      :require_context => Section,
                       :parent => :author,
                       :message => 'You need to be a moderator to perform this action.'
 
     Rbac::Role.define :admin,
-                      :require_context => Site, 
+                      :require_context => Site,
                       :parent => :moderator,
                       :message => 'You need to be an admin to perform this action.'
 
     # Rbac::Role.define :owner,
-    #                   :require_context => Account, 
+    #                   :require_context => Account,
     #                   :parent => :admin,
     #                   :message => 'You need to be the owner of this account to perform this action.'
 
-    Rbac::Role.define :superuser, 
+    Rbac::Role.define :superuser,
                       :parent => :admin, #:owner,
                       :message => 'You need to be a superuser to perform this action.'
   end
-  
+
   def include_role(name, options = {})
     role = Rbac::Role.build name, options
     simple_matcher do |given, matcher|
@@ -44,7 +44,7 @@ module SpecRolesHelper
       given.include? role
     end
   end
-  
+
   def have_role(name, options = {})
     simple_matcher do |given, matcher|
       msg = "role #{name.inspect}" + (options[:context] ? " in the context of #{options[:context]}" : '')
@@ -58,34 +58,34 @@ end
 
 class RbacExampleGroup < Spec::Example::ExampleGroup
   include SpecRolesHelper
-  
+
   before :each do
     backup_role_classes
     remove_role_classes
-    
+
     Rbac::Context.permissions = { :'create article' => :superuser }
 
     #@account = Account.new
     @site = Site.new #:account => @account
     @section = Section.new :site => @site
     @content = Content.new :section => @section
-    
+
     @site_2 = Site.new #:account => @account
     @section_2 = Section.new :site => @site_2
-    
+
     #@other_account = Account.new
     @other_site = Site.new #:account => @other_account
     @other_section = Section.new :site => @other_site
-    @other_content = Content.new :section => @other_section    
+    @other_content = Content.new :section => @other_section
 
     @user = User.new #:account => @account
   end
-  
+
   after :each do
     remove_role_classes
     restore_role_classes
   end
-  
+
   module RoleBackup
     mattr_accessor :children, :permissions
   end
@@ -97,14 +97,14 @@ class RbacExampleGroup < Spec::Example::ExampleGroup
     RoleBackup.children = Rbac::Role::Base.children
     RoleBackup.permissions = Rbac::Context.permissions
   end
-  
+
   def remove_role_classes
     Rbac::Role.constants.each do |name|
       Rbac::Role.send :remove_const, name unless name == "Base"
     end
     Rbac::Role::Base.children = []
   end
-  
+
   def restore_role_classes
     RoleBackup.constants.each do |name|
       Rbac::Role.const_set name, RoleBackup.const_get(name)
