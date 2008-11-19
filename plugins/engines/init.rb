b5c704e0bfa7871b7ff333557d9a5937969c5fe1
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-19 14:35:56 +0100

    upgraded the engines plugin

--- plugins/engines/init.rb
+++ plugins/engines/init.rb
@@ -3,7 +3,3 @@
 config.after_initialize do
   Engines.init if defined? :Engines
 end
-
-# reverted that change because it gets me into a stack level to deep error, see comment 
-# on http://github.com/lazyatom/engines/commit/f7656144b71685c06cfc04dbf41825358646b466
-# Engines.init if defined? :Engines
