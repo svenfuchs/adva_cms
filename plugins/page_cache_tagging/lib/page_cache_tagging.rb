6264474bc816f320b59806c882555e73b70bebf2
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-20 15:26:29 +0100

    remove asset_tag caching for now

--- plugins/page_cache_tagging/lib/page_cache_tagging.rb
+++ plugins/page_cache_tagging/lib/page_cache_tagging.rb
@@ -67,7 +67,7 @@ ActionController::Base.class_eval do
 
   def expire_site_page_cache
     cache_dir = page_cache_directory
-    if cache_dir =~ /\/public$/ 
+    if cache_dir.gsub('/', '') =~ /public$/ 
       # TODO can not simply kill the whole cache dir unless in multisite mode
       # this misses assets as stylesheets from themes though because they are
       # not referenced as cached, yet
