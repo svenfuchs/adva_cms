ae417227c22133209b015430b098d7400b98e527
Marko Seppae <marko.seppa@gmail.com>
2008-11-20 13:06:29 +0100

    fixed user profile links on _utility, _navigation and _menu_global

--- engines/adva_cms/spec/views/widgets_spec.rb
+++ engines/adva_cms/spec/views/widgets_spec.rb
@@ -12,7 +12,7 @@ describe "Widgets:", "the admin/menu_global widget" do
 
     helpers = [:admin_global_select_tag, :admin_site_select_tag,
                :admin_plugins_path, :admin_site_user_path,
-               :admin_user_path, :logout_path]
+               :admin_user_path, :logout_path, :link_to_profile]
     helpers.each do |helper| template.stub!(helper).and_return helper.to_s end
   end
   
@@ -49,17 +49,42 @@ describe "Widgets:", "the admin/menu_global widget" do
   end
 
   it "shows the current user's name" do
+    template.should_receive(:link_to_profile).with(nil, :name => @user.name).and_return('link to profile')
     act!
-    response.should have_text(/#{@user.name}/)
   end
 
   it "shows a link to the current user profile" do
+    template.should_receive(:link_to_profile)
     act!
-    response.should have_tag('a[href=?]', "admin_user_path")
   end
 
   it "shows a logout link" do
     act!
     response.should have_tag('a[href=?]', "logout_path")
+  end
+end
+
+describe "_navigation.html.erb" do
+  before :each do
+    template.stub!(:link_to).and_return('link')
+    @site = mock_model(Site, :id => 1, :new_record? => false)
+  end
+  
+  it "should use link_to_profile helper" do
+    template.should_receive(:link_to_profile).and_return('link to profile')
+    render 'widgets/admin/_navigation'
+  end
+end
+
+describe "_utility.html.erb" do
+  before :each do
+    @user = mock_model(User, :id => 1, :name => 'Dummy')
+    template.stub!(:current_user).and_return(@user)
+    template.stub!(:link_to).and_return('link')
+  end
+  
+  it "should use link_to_profile helper" do
+    template.should_receive(:link_to_profile).with(nil, :name => @user.name).and_return('link to profile')
+    render 'widgets/admin/_utility'
   end
 end
