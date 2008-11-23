a36c7c2200024d575888488f1463f127558142b9
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-23 22:13:56 +0100

    fix page cache tagging: tracking was a bit too defensive now ;)

--- plugins/page_cache_tagging/lib/page_cache_tagging/record_read_access_tracker.rb
+++ plugins/page_cache_tagging/lib/page_cache_tagging/record_read_access_tracker.rb
@@ -1,7 +1,7 @@
 module PageCacheTagging
   class RecordReadAccessTracker
     attr_reader :references
-  
+
     def initialize(owner, options)
       @references = []
 
@@ -13,14 +13,14 @@ module PageCacheTagging
         end
       end
     end
-  
+
     def resolve_trackable(owner, trackable)
       case trackable
         when Symbol then owner.send trackable
         when /^@/   then owner.instance_variable_get(trackable)
       end
     end
-  
+
     def track(trackable, methods)
       methods = [methods] if methods && !methods.is_a?(Array)
       if trackable.is_a? Array
@@ -30,17 +30,17 @@ module PageCacheTagging
         track_methods(trackable, methods)
       end
     end
-  
+
     def track_attributes(trackable, methods)
-      attributes = methods ? methods.select{|method| trackable.has_attribute? method } : nil    
+      attributes = methods ? methods.select{|method| trackable.has_attribute? method } : nil
       RecordAttributesReadObserver.new(trackable, attributes).register(self) if methods.nil? || attributes
     end
-  
+
     def track_methods(trackable, methods)
       non_attributes = methods ? methods.select{|method| !trackable.has_attribute? method } : nil
       MethodReadObserver.new(trackable, non_attributes).register(self) if non_attributes && !non_attributes.empty?
     end
-  
+
     def notify(object, method = nil)
       @references << [object, method]
     end
