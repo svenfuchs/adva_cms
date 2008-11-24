938d57e48bf5b383025e039af8f6c7cbda5fc810
Sven Fuchs <svenfuchs@artweb-design.de>
2008-11-24 13:19:40 +0100

    make sure creating/destroying sections invalidates the page cache per site

--- engines/adva_cms/app/observers/section_sweeper.rb
+++ engines/adva_cms/app/observers/section_sweeper.rb
@@ -1,9 +1,13 @@
 class SectionSweeper < PageCacheTagging::Sweeper
   observe Section
+  
+  def after_create(section)
+    expire_cached_pages_by_site(section.site)
+  end
 
   def after_save(section)
     expire_cached_pages_by_section(section)
   end
 
-  alias after_destroy after_save
+  alias after_destroy after_create
 end
