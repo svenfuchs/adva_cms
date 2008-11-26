4ed8548e049c6a05b1a74914b82d9639bc4940ca
Thomas R. Koll <tomk32@tomk32.de>
2008-11-26 09:27:58 +0100

    started with Calendar::Event

---
+++ engines/adva_calendar/app/models/calendar/event.rb
@@ -0,0 +1,15 @@
+class Calendar::Event < ActiveRecord::Base
+  filters_attributes :sanitize => :body_html, :except => [:body, :cached_tag_list]
+  before_create :set_published
+  set_table_name :calendar_events
+
+  def after_initialize
+    self.title = permalink.to_s.gsub("-", " ").capitalize if new_record? && title.blank? && permalink
+  end
+
+  def set_published
+    self.published_at = Time.zone.now
+  end
+
+
+end
--- engines/adva_calendar/spec/models/calendar/event_spec.rb
+++ engines/adva_calendar/spec/models/calendar/event_spec.rb
@@ -1,11 +1,11 @@
-require File.dirname(__FILE__) + '/../spec_helper'
+require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe Calendar::Event do
-  include Stubby, Matchers::ClassExtensions
+describe Event do
+  include Matchers::ClassExtensions
 
   before :each do
-    @calendar = stub_calendar
-    @event = @calendar.events.new :author => stub_user
+    @calendar = Calendar.create!(:title => 'Concerts')
+    @event = @calendar.events.create!
   end
 
   describe 'class extensions:' do
@@ -22,7 +22,7 @@ describe Calendar::Event do
     it 'sets its published_at attribute to the current time before create' do
       Calendar::Event.before_create.should include(:set_published)
     end
-
+  end
   describe '#accept_comments?' do
     it 'does not accept comments' do
       @event.should_receive(:accept_comments?).and_return false
@@ -35,15 +35,24 @@ describe Calendar::Event do
   end
   
   describe "relations" do
-    it "should have a host"
-    it "should have a location (country, city, adress)"
+    it "should belong to a category"
+    it "should belong to a location (country, city, adress)"
     it "should have user bookmarks"
+    it "should have tags"
   end
   
   describe "named scopes" do
-    it "should have a recent scope"
+    it "should have a elapsed scope"
     it "should have a upcoming scope"
     it "should have a recently added scope"
   end
   
+  describe "recurring events" do
+    it "should have a parent event"
+    it "should support daily events"
+    it "should support weekly events"
+    it "should support monthly events"
+    it "should support yearly events"
+    it "should support weekdays"
+  end
 end
