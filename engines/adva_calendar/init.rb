f28188fcb9743a3094c7f114e6ad56cf0a606727
Thomas R. Koll <tomk32@tomk32.de>
2008-11-26 09:27:58 +0100

    started with Calendar::Event

--- engines/adva_calendar/init.rb
+++ engines/adva_calendar/init.rb
@@ -1,6 +1,9 @@
 # remove plugin from load_once_paths 
 ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
 
+ActiveSupport::Dependencies.load_once_paths += %W(#{ File.dirname(__FILE__) }app/models/calendar/)
+
 config.to_prepare do
   Section.register_type 'Calendar'
+  require 'calendar/event'
 end
