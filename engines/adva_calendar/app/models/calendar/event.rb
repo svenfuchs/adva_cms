f6b0118f01060761a4d9368f1444bc73d1794577
Thomas R. Koll <tomk32@tomk32.de>
2008-11-26 14:46:21 +0100

    add named scope recently_added

--- engines/adva_calendar/app/models/calendar/event.rb
+++ engines/adva_calendar/app/models/calendar/event.rb
@@ -11,6 +11,7 @@ class Calendar::Event < ActiveRecord::Base
   
   named_scope :elapsed, lambda {{:conditions => ['startdate < ? AND (enddate IS ? OR enddate < ?)', Time.now, nil, Time.now], :order => 'enddate DESC'}}
   named_scope :upcoming, Proc.new {|date| {:conditions => ['startdate > ? OR (startdate < ? AND enddate > ?)', date||Time.now, date||Time.now, date||Time.now], :order => 'startdate ASC'}}
+  named_scope :recently_added, lambda{{:conditions => ['startdate > ? OR (startdate < ? AND enddate > ?)', Time.now, Time.now, Time.now], :order => 'created_at DESC'}}
 
   def set_published
     self.published_at = Time.zone.now
