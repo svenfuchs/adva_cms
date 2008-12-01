058cc4a609f76ce687d2354d1a856dad13b678e8
Thomas R. Koll <tomk32@tomk32.de>
2008-12-01 23:56:39 +0100

    more on calendar routes

--- engines/adva_calendar/routes.rb
+++ engines/adva_calendar/routes.rb
@@ -5,11 +5,13 @@ end
 with_options :controller => 'events', :action => 'index', :requirements => { :method => :get } do |event|
   event.events "events/:section_id/:year/:month/:day", :year => nil, :month => nil, :day => nil,
     :requirements => { :year => /\d{4}/, :month => /\d{1,2}/ }
-  event.events_category "events/:section_id/categories/:category_id"
   event.formatted_events "events/:section_id.:format"
+
+  event.events_category "events/:section_id/categories/:category_id"
   event.formatted_events_category "events/:section_id/categories/:category_id.:format"
   
   event.event "/event/:section_id/:id", :action => 'show'
+  event.event "/event/:section_id/:id.:format", :action => 'show'
 end
 
 
