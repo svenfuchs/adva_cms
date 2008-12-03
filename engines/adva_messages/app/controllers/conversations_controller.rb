76b9f20f6d3b5886539f267d72d5e17866d813d4
Marko Seppae <marko.seppa@gmail.com>
2008-12-03 15:38:18 +0100

    flash messages added

--- engines/adva_messages/app/controllers/conversations_controller.rb
+++ engines/adva_messages/app/controllers/conversations_controller.rb
@@ -23,6 +23,8 @@ class ConversationsController < BaseController
     def set_conversation
       @conversation = current_user.conversations.find(params[:id])
     rescue
+      flash[:error] = "Requested conversation could not be found"
+      write_flash_to_cookie # TODO make around filter or something
       redirect_to messages_path
     end
 end
