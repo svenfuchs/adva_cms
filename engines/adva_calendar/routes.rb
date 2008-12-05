3f7c34c9d7831cf357b3ab5bf5a2c4c398966cb3
Thomas R. Koll <tomk32@tomk32.de>
2008-12-05 11:32:54 +0100

    ah, first the index routes then show

--- engines/adva_calendar/routes.rb
+++ engines/adva_calendar/routes.rb
@@ -1,8 +1,4 @@
 
-with_options :controller => 'events', :action => 'show', :requirements => { :method => :get } do |event|
-  event.calendar_event "/calendars/:section_id/:id"
-  event.formatted_calendar_event "/calendars/:section_id/:id.:format"
-end
 
 with_options :controller => 'events', :action => 'index', :requirements => { :method => :get } do |event|
   event.calendar_events "calendars/:section_id/:year/:month/:day", :year => nil, :month => nil, :day => nil,
@@ -13,6 +9,10 @@ with_options :controller => 'events', :action => 'index', :requirements => { :me
   event.formatted_calendar_events_category "calendars/:section_id/categories/:category_id.:format"
 end
 
+with_options :controller => 'events', :action => 'show', :requirements => { :method => :get } do |event|
+  event.calendar_event "/calendars/:section_id/:id"
+  event.formatted_calendar_event "/calendars/:section_id/:id.:format"
+end
 
 map.resources :calendar_events, :controller  => 'admin/events',
                                 :path_prefix => 'admin/sites/:site_id/sections/:section_id',
