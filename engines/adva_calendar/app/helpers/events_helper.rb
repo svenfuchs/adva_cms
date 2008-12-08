547a317c653ef4aaa85b4d3d16ef37131888de79
Thomas R. Koll <tomk32@tomk32.de>
2008-12-08 10:35:16 +0100

    adjust link_to_event to new routes

--- engines/adva_calendar/app/helpers/events_helper.rb
+++ engines/adva_calendar/app/helpers/events_helper.rb
@@ -12,6 +12,6 @@ module EventsHelper
   end
   
   def link_to_event(event)
-    link_to event.title, event_url(event.section, event.permalink)
+    link_to event.title, calendar_event_url(event.section, event.permalink)
   end
 end
