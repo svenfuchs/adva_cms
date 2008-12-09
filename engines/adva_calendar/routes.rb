c5109eb0a13b17ef3169cd1f01070272b624d870
Thomas R. Koll <tomk32@tomk32.de>
2008-12-09 10:01:34 +0100

    Working on calendar front views

--- engines/adva_calendar/routes.rb
+++ engines/adva_calendar/routes.rb
@@ -5,14 +5,14 @@ with_options :controller => 'events', :action => 'index', :requirements => { :me
     :requirements => { :year => /\d{4}/, :month => /\d{1,2}/ }
   event.formatted_calendar_events "calendars/:section_id.:format"
 
-  event.calendar_events_category "calendars/:section_id/categories/:category_id"
-  event.formatted_calendar_events_category "calendars/:section_id/categories/:category_id.:format"
+  event.calendar_category "calendars/:section_id/categories/:category_id"
+  event.formatted_calendar_category "calendars/:section_id/categories/:category_id.:format"
+  
+  event.calendar_tag 'calendars/:section_id/tags/:tags', :requirements => { :method => :get }
 end
 
-with_options :controller => 'events', :action => 'show', :requirements => { :method => :get } do |event|
-  event.calendar_event "/calendars/:section_id/:id"
-  event.formatted_calendar_event "/calendars/:section_id/:id.:format"
-end
+map.calendar_event "calendars/:section_id/:id", :controller => 'events', :action => 'show', :requirements => { :method => :get }
+map.formatted_calendar_event "calendars/:section_id/:id.:format", :controller => 'events', :action => 'show', :requirements => { :method => :get }
 
 map.resources :calendar_events, :controller  => 'admin/events',
                                 :path_prefix => 'admin/sites/:site_id/sections/:section_id',
