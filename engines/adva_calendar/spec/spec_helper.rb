9b980f255dd613f8fb9d9dbe8f90cde6977eb33e
Thomas R. Koll <tomk32@tomk32.de>
2008-12-11 10:00:35 +0100

    cleaning up

--- engines/adva_calendar/spec/spec_helper.rb
+++ engines/adva_calendar/spec/spec_helper.rb
@@ -2,6 +2,3 @@ require File.dirname(__FILE__) + '/../../adva_cms/spec/spec_helper'
 
 
 require File.dirname(__FILE__) + '/../lib/cms_extensions'
-Spec::Runner.configure do |config|
-#  config.fixture_path = File.dirname(__FILE__) + '/fixtures/'
-end
