e42b2a215f0b4f7786ae0d1e5f36a911b81f11c5
Thomas R. Koll <tomk32@tomk32.de>
2008-12-14 14:09:38 +0100

    renamed migration

--- engines/adva_assets/db/migrate/20081212113000_add_content_type_to_asset_assignments.rb
+++ engines/adva_assets/db/migrate/20081212113000_add_content_type_to_asset_assignments.rb
@@ -1,7 +1,7 @@
 class AddContentTypeToAssetAssignments < ActiveRecord::Migration
   def self.up
     # a safety net if previously migrated. sorry
-    return if AssetAssingment.new.attributes_before_type_cast.has_key?('content_type')
+    return if AssetAssignment.new.attributes_before_type_cast.has_key?('content_type')
     add_column :asset_assignments, :content_type, :string
     execute 'UPDATE asset_assignments SET content_type = (SELECT type from contents WHERE contents.id = asset_assignments.content_id)'
   end
