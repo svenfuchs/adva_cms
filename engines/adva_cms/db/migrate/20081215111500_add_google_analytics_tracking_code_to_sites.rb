1f611c2f782d83fce40deb9fa0da7906673a69f8
Clemens Kofler <clemens@railway.at>
2008-12-15 12:57:38 +0100

    Include Analytics tracking code in site settings.

---
+++ engines/adva_cms/db/migrate/20081215111500_add_google_analytics_tracking_code_to_sites.rb
@@ -0,0 +1,9 @@
+class AddGoogleAnalyticsTrackingCodeToSites < ActiveRecord::Migration
+  def self.up
+    add_column :sites, :google_analytics_tracking_code, :string
+  end
+
+  def self.down
+    remove_column :sites, :google_analytics_tracking_code
+  end
+end
