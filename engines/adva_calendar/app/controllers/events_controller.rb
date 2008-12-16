68fb758f6451b46936078a46e0cbcba09b20f312
Thomas R. Koll <tomk32@tomk32.de>
2008-12-16 13:18:37 +0100

    only displaying published events in calendar front view

--- engines/adva_calendar/app/controllers/events_controller.rb
+++ engines/adva_calendar/app/controllers/events_controller.rb
@@ -13,17 +13,18 @@ class EventsController < BaseController
 
   def index
     # a bit restricting, I know
+    source = @section.events.published
     if %w(title body).include?(params[:filter])
-      @events = @section.events.search(params[:query], params[:filter]).paginate({:page => params[:page]})
-    elsif params[:filter] == 'tags'
-      @events = @section.events.paginate_tagged_with(params[:query], :page => params[:page])
+      @events = source.search(params[:query], params[:filter]).paginate({:page => params[:page]})
+    elsif params[:filter] == 'tags' and not params[:query].blank?
+      @events = source.paginate_tagged_with(params[:query], :page => params[:page])
     else
       if @category 
-        @events = @section.events.by_categories(@category.id).paginate(:page => params[:page])
+        @events = source.by_categories(@category.id).paginate(:page => params[:page])
       else
-        @events = @section.events.elapsed.paginate({:page => params[:page]}) if params[:elapsed]
-        @events = @section.events.recent.paginate({:page => params[:page]}) if params[:recent] and @events.blank?
-        @events ||=  @section.events.upcoming(@timespan).paginate({:page => params[:page]})
+        @events = source.elapsed.paginate({:page => params[:page]}) if params[:elapsed]
+        @events = source.recent.paginate({:page => params[:page]}) if params[:recent] and @events.blank?
+        @events ||= source.upcoming(@timespan).paginate({:page => params[:page]})
       end
     end
     respond_to do |format|
