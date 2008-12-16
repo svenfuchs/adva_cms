3b7a6c461efa8ca7745b78d993cae50a826489c6
Thomas R. Koll <tomk32@tomk32.de>
2008-12-16 15:15:56 +0100

    another spec for permalinks in calendar

--- engines/adva_calendar/spec/controllers/events_routes_spec.rb
+++ engines/adva_calendar/spec/controllers/events_routes_spec.rb
@@ -17,6 +17,7 @@ describe EventsController do
       route.it_maps :get, "/calendars/1", :index
       route.it_maps :get, "/calendars/1/categories/2", :index, :category_id => "2"
       route.it_maps :get, "/calendars/1/event/2008", :show, :id => "2008"
+      route.it_maps :get, "/calendars/1/event/a-jazz-concert", :show, :id => 'a-jazz-concert'
       route.it_maps :get, "/calendars/1/events/2008", :index, :year => "2008"
       route.it_maps :get, "/calendars/1/events/2008/11", :index, :year => "2008", :month => "11"
       route.it_maps :get, "/calendars/1/events/2008/11/27", :index, :year => "2008", :month => "11", :day => "27"
