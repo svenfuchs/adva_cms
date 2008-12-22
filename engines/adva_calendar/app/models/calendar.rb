abb79b65dcaf0e8087b590786894e8438a9ac3aa
Thomas R. Koll <tomk32@tomk32.de>
2008-12-22 13:25:31 +0100

    added another _calendar partial with CalendarHelper add Calendar.days_in_month_with_events

--- engines/adva_calendar/app/models/calendar.rb
+++ engines/adva_calendar/app/models/calendar.rb
@@ -6,4 +6,9 @@ class Calendar < Section
       'CalendarEvent'
     end
   end
+  def days_in_month_with_events(date)
+    events.find(:all, 
+        :select => 'startdate', :order => 'startdate ASC',
+        :conditions => ['startdate > ? and startdate < ?', date.beginning_of_month, date.end_of_month]).collect{|e| e.startdate.to_date}
+  end
 end
--- engines/adva_calendar/spec/models/calendar_spec.rb
+++ engines/adva_calendar/spec/models/calendar_spec.rb
@@ -2,7 +2,11 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Calendar do
   before :each do
-    @calendar = Calendar.new
+    @calendar = Calendar.new(:title => 'A calendar')
+  end
+  
+  it "should be valid" do
+    @calendar.should be_valid
   end
   
   it "is a kind of Section" do
@@ -21,4 +25,21 @@ describe Calendar do
     Calendar.content_type.should == 'CalendarEvent'
   end
   
+  describe ":days_in_month_with_events" do
+    before do
+      args = {:user_id => 1, :title => 'Event', :location_id => 1}
+      @calendar_with_events = Calendar.create!(:title => 'Another calendar')
+      @event1 = @calendar_with_events.events.create!(args.merge(:startdate => '2008-11-27'))
+      @event2 = @calendar_with_events.events.create!(args.merge(:startdate => '2008-11-24', :enddate => '2008-11-26'))
+      @event3 = @calendar_with_events.events.create!(args.merge(:startdate => '2007-11-27'))
+      @event4 = @calendar_with_events.events.create!(args.merge(:startdate => '2008-10-30', :enddate => '2008-11-02'))<
+    end
+    it "should be empty for an empty month" do
+      @calendar_with_events.days_in_month_with_events(Date.civil(1999,12)).should be_empty
+    end
+    it "should return a list for months with events" do
+      @calendar_with_events.days_in_month_with_events(Date.civil(2008,11)).should ==[Date.civil(2008,11,24), Date.civil(2008,11,27)]
+      @calendar_with_events.days_in_month_with_events(Date.civil(2008,10)).should ==[Date.civil(2008,10,30)]
+    end
+  end
 end
