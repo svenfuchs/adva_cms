e208efeaa4ffa8a5ae78cd5e35980e5a6f8acb56
Marko Seppae <marko.seppa@gmail.com>
2008-12-22 11:43:56 +0100

    implemented photo sweeper, page caching for photo album works now

---
+++ engines/adva_photos/app/observers/photo_sweeper.rb
@@ -0,0 +1,13 @@
+class PhotoSweeper < PageCacheTagging::Sweeper
+  observe Photo
+  
+  def after_create(photo)
+    expire_cached_pages_by_section(photo.section)
+  end
+
+  def before_save(photo)
+    expire_cached_pages_by_reference(photo)
+  end
+
+  alias after_destroy before_save
+end
