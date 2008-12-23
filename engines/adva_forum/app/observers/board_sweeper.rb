634079cfe1316412084238501fa95c3ca9e0870b
Marko Seppae <marko.seppa@gmail.com>
2008-12-23 11:51:28 +0100

    board sweeper implemented

---
+++ engines/adva_forum/app/observers/board_sweeper.rb
@@ -0,0 +1,13 @@
+class BoardSweeper < PageCacheTagging::Sweeper
+  observe Board
+  
+  def after_create(board)
+    expire_cached_pages_by_section(board.section)
+  end
+
+  def before_save(board)
+    expire_cached_pages_by_reference(board)
+  end
+
+  alias after_destroy before_save
+end
