ccdcf10e008b87339d2b7682d1a3d023140094aa
Thomas R. Koll <tomk32@tomk32.de>
2008-12-23 10:04:23 +0100

    fixed "apply tags to all" in assets upload form

--- engines/adva_cms/public/javascripts/admin/asset.js
+++ engines/adva_cms/public/javascripts/admin/asset.js
@@ -33,7 +33,7 @@ Asset = {
   },
 
 	applyTagsToAll: function(form_id) {
-    var inputs = $(form_id).getInputs('text', 'assets[][tag_list]');
+    var inputs = $(form_id).getInputs('text', 'assets[0][tag_list]');
     var tags = $F(inputs.first()).split(' ');
     tags = tags.collect(function(t) { return t.strip(); });
     inputs.each(function(e, index) {
