1675bacc13b79d6bd42c74575e0467661ba3e894
priit.mx.ee <priit@mx.ee>
2009-01-06 17:15:04 +0200

    included FactoryScenario to top factories.rb

--- engines/adva_cms/factories/factories.rb
+++ engines/adva_cms/factories/factories.rb
@@ -1,6 +1,7 @@
 $:.push File.expand_path(RAILS_ROOT + '/vendor/gems/thoughtbot-factory_girl-1.1.3/lib')
 require 'factory_girl'
 require 'factory_girl/scenarios'
+include FactoryScenario
 
 Spec::Example::ExampleGroup.send :include, FactoryScenario if defined? Spec
 
