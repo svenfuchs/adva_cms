733252011e77871785a3ec09c66fa3a8e69124d0
Marko Seppae <marko.seppa@gmail.com>
2009-01-08 14:30:27 +0100

    some integration tests for topics

---
+++ engines/adva_forum/test/integration/topics_common.rb
@@ -0,0 +1,146 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '..', 'test_helper' ))
+
+class TopicsCommon < ActionController::IntegrationTest
+  def setup
+    # Note to self! (Again) login_as deletes all the users, so post.author
+    # does not work anymore. Thats why it has to come before scenario.
+    login_as          :admin
+    factory_scenario  :forum_with_topics
+  end
+  
+  def test_an_admin_edits_the_existing_topic
+    assert @topic.title != 'Updated test topic'
+    
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @topic.title
+    assert_template 'topics/show'
+    
+    click_link 'Edit'
+    
+    fill_in       'Title', :with => 'Updated test topic'
+    click_button  'Save'
+    
+    assert_template 'topics/show'
+    @topic.reload
+    assert @topic.title == 'Updated test topic'
+  end
+  
+  def test_an_admin_makes_the_topic_sticky
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @topic.title
+    assert_template 'topics/show'
+    
+    click_link 'Edit'
+    
+    check         'Sticky'
+    click_button  'Save'
+    
+    assert_template 'topics/show'
+    @topic.reload
+    assert @topic.sticky?
+  end
+  
+  def test_an_admin_locks_the_topic
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @topic.title
+    assert_template 'topics/show'
+    
+    click_link 'Edit'
+    
+    check         'Locked'
+    click_button  'Save'
+    
+    assert_template 'topics/show'
+    @topic.reload
+    assert @topic.locked?
+  end
+  
+  def test_an_admin_deletes_the_existing_topic
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @topic.title
+    assert_template 'topics/show'
+    
+    assert Topic.count == 2
+    
+    click_link 'Delete'
+    
+    assert_template 'forum/show'
+    assert Topic.count == 1
+  end
+  
+  def test_an_admin_goes_to_previous_topic_when_on_first_topic
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @forum.topics.first.title
+    assert_template 'topics/show'
+    
+    click_link 'Previous'
+    
+    assert_template 'topics/show'
+    assert_flash  'There is no previous topic'
+  end
+  
+  def test_an_admin_goes_to_previous_topic_when_on_last_topic
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @forum.topics.last.title
+    assert_template 'topics/show'
+    
+    click_link 'Previous'
+    
+    assert_template 'topics/show'
+    
+    assert_select "div#main" do
+      assert @forum.topics.first.title == 'Test topic'
+      assert_select "h2", /Test topic/
+    end
+  end
+  
+  def test_an_admin_goes_to_next_topic_when_on_last_topic
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @forum.topics.last.title
+    assert_template 'topics/show'
+    
+    click_link 'Next'
+    
+    assert_template 'topics/show'
+    assert_flash  'There is no next topic'
+  end
+  
+  def test_an_admin_goes_to_next_topic_when_on_first_topic
+    # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the board
+    click_link @forum.topics.first.title
+    assert_template 'topics/show'
+    
+    click_link 'Next'
+    
+    assert_template 'topics/show'
+    
+    assert_select "div#main" do
+      assert @forum.topics.last.title == 'Test topic'
+      assert_select "h2", /Test topic/
+    end
+  end
+end
