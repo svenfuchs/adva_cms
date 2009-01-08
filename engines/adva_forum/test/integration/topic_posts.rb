2177fe38b828c46fad00bbfb53d3ab40638e02a6
Marko Seppae <marko.seppa@gmail.com>
2009-01-08 15:33:21 +0100

    topic post integration tests

--- engines/adva_forum/test/integration/topic_posts.rb
+++ engines/adva_forum/test/integration/topic_posts.rb
@@ -4,17 +4,13 @@ class TopicPosts < ActionController::IntegrationTest
   def setup
     factory_scenario  :site_with_forum
     login_as          :admin
-    @board  = Factory :board, :site => @site, :section => @forum
-    @topic  = @forum.topics.post(@user, Factory.attributes_for(:topic, :section => @forum, :board => @board))
+    @topic  = @forum.topics.post(@user, Factory.attributes_for(:topic, :section => @forum))
     @topic.save
   end
   
   def test_an_admin_creates_a_new_post_to_a_topic
      # Go to section
     get forum_path(@forum)
-    
-    # Admin clicks link to go to the board
-    click_link @board.title
     
     # Admin clicks link to go to the topic
     click_link @topic.title
@@ -28,5 +24,46 @@ class TopicPosts < ActionController::IntegrationTest
     assert Post.count == 2
     
     assert_template 'topics/show'
+  end
+  
+  def test_an_admin_deletes_an_existing_topic_post
+    post = @topic.reply(@user, :body => 'Delete me')
+    post.save
+     # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the topic
+    click_link @topic.title
+    
+    assert_template 'topics/show'
+    assert @topic.comments.size == 2
+    
+    click_link  "delete_comment_#{post.id}"
+    
+    @topic.comments.reload
+    assert @topic.comments.size == 1
+    
+    assert_template 'topics/show'
+  end
+  
+  def test_an_admin_edits_an_existing_topic_post
+     # Go to section
+    get forum_path(@forum)
+    
+    # Admin clicks link to go to the topic
+    click_link @topic.title
+    assert_template 'topics/show'
+    
+    # Admin clicks link to go to the post edit form
+    click_link "edit_comment_#{@topic.initial_post.id}"
+    assert @topic.initial_post.body != 'Updated test post'
+    
+    assert_template 'posts/edit'
+    fill_in       'post[body]', :with => 'Updated test post'
+    click_button  'Save post'
+    
+    assert_template 'topics/show'
+    @topic.initial_post.reload
+    assert @topic.initial_post.body == 'Updated test post'
   end
 end
