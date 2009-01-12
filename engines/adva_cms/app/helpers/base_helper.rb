7347af9ad6f792401bc19cd8b36c6b58ca5239f0
Clemens Kofler <clemens@railway.at>
2009-01-12 13:27:18 +0100

    Accidentally left test method in the code. ;-)

--- engines/adva_cms/app/helpers/base_helper.rb
+++ engines/adva_cms/app/helpers/base_helper.rb
@@ -39,31 +39,6 @@ module BaseHelper
     concat lines.join("\n")
   end
 
-  def test_form_for(record_or_name_or_array, *args, &proc)
-    raise ArgumentError, "Missing block" unless block_given?
-
-    options = args.extract_options!
-
-    case record_or_name_or_array
-    when String, Symbol
-      object_name = record_or_name_or_array
-    when Array
-      object = record_or_name_or_array.last
-      object_name = ActionController::RecordIdentifier.singular_class_name(object)
-      apply_form_for_options!(record_or_name_or_array, options)
-      args.unshift object
-    else
-      object = record_or_name_or_array
-      object_name = ActionController::RecordIdentifier.singular_class_name(object)
-      apply_form_for_options!([object], options)
-      args.unshift object
-    end
-
-    concat(form_tag(options.delete(:url) || {}, options.delete(:html) || {}))
-    fields_for(object_name, *(args << options), &proc)
-    concat('</form>')
-  end
-
   # # same as Rails text helper, but returns only the pluralized string without
   # # the number botched into it
   # def pluralize_str(count, singular, plural = nil)
