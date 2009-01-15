5932ad7f5ab492d96329d7452ac399dfd40fcd00
Thomas R. Koll <tomk32@tomk32.de>
2009-01-15 14:08:49 +0100

    fixed build

--- engines/adva_user/spec/views/admin/user_views_spec.rb
+++ engines/adva_user/spec/views/admin/user_views_spec.rb
@@ -75,7 +75,7 @@ describe "Admin::User:" do
 
     describe "with the current user not having permissions to manage roles" do
       it "does not render the roles partial" do
-        # template.should_receive(:has_permission?).with('manage', 'roles').and_return false
+        template.should_receive(:has_permission?).with('manage', 'roles').and_return false
         template.should_not_receive(:render).with hash_including(:partial => 'roles')
         render "admin/users/edit"
       end
