10e55a6c4def8cb88ab0a9362d3f2822e641976e
Sven Fuchs <svenfuchs@artweb-design.de>
2009-01-15 20:29:00 +0100

    stop Webrat from hijacking Safari for error messages

--- engines/adva_cms/test/test_helper.rb
+++ engines/adva_cms/test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../../../../../config/enviro
 require 'test_help'
 
 require File.expand_path(File.dirname(__FILE__) + "/../../../spec/webrat/lib/webrat/rails")
+Webrat.configuration.open_error_files = false
 
 require File.expand_path(File.join(File.dirname(__FILE__), '..', 'factories', 'factories'))
 require File.expand_path(File.join(File.dirname(__FILE__), 'test_helpers', 'assertions'))
