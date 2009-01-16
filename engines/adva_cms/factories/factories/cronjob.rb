005939a2b55da8623199081274d7bcdf6a339890
priit.mx.ee <priit@mx.ee>
2009-01-16 10:41:15 +0200

    rm ar_mailer gem because it's no needed

--- engines/adva_cms/factories/factories/cronjob.rb
+++ engines/adva_cms/factories/factories/cronjob.rb
@@ -4,5 +4,5 @@ end
 
 Factory.define :cronjob_with_exact_due_datetime, :class => Cronjob do |c|
   c.command "test_command"
-  c.due_at DateTime.new 2008,01,15,20,00
+  c.due_at DateTime.new(2008,01,15,20,00)
 end
