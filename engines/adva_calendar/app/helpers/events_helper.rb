889390dc01fba649226e293a0740470da47f7722
Thomas R. Koll <tomk32@tomk32.de>
2009-01-19 17:24:01 +0100

    i18n EventsHelper

--- engines/adva_calendar/app/helpers/events_helper.rb
+++ engines/adva_calendar/app/helpers/events_helper.rb
@@ -2,9 +2,9 @@ module EventsHelper
   def collection_title(category=nil, tags=nil)
     title = []
     title << current_timespan.first.strftime(current_timespan_format||t(:'adva.calendar.titles.formats.year_month')) unless current_timespan.first.blank?
-    title << "in #{category.title}" if category
-    title << "tagged #{tags.to_sentence}" if tags
-    'Events ' + title.join(', ') unless title.empty?
+    title << t(:'adva.calendar.titles.in_category', :title => category.title) if category
+    title << t(:'adva.calendar.titles.tagged', :tags => tags.to_sentence) if tags
+    t(:'adva.calendar.titles.events') + ' ' + title.join(', ') unless title.empty?
   end
   
   def link_to_event(event)
