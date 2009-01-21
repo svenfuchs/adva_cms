d9d5ade3aa7ab3d4e6a9def7208bce133012f878
Thomas R. Koll <tomk32@tomk32.de>
2009-01-21 12:15:18 +0100

    added a integration test for calendar front views

--- engines/adva_cms/factories/factories/calendar_event.rb
+++ engines/adva_cms/factories/factories/calendar_event.rb
@@ -1,11 +1,12 @@
 Factory.sequence :calendar_event do |n|
-  "Calendar #{n}"
+  "Calendar Event #{n}"
 end
 
 Factory.define :calendar_event do |c|
   c.title { Factory.next :calendar_event }
-  c.start_date '2008-11-27 12:00'
-  c.end_date '2008-11-27 15:00'
+  c.start_date Time.now
+  c.end_date Time.now + 2.hours
+  c.published_at Time.now
   c.user_id 1
   c.location_id 1
 end
