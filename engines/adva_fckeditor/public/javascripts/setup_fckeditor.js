4808ca79bab9663f955c2c0e92194bd33f29ed93
Clemens Kofler <clemens@railway.at>
2009-01-26 17:12:40 +0100

    Attach FCKeditor to Filter select so that FCKeditor is only used for plain HTML filter.

--- engines/adva_fckeditor/public/javascripts/setup_fckeditor.js
+++ engines/adva_fckeditor/public/javascripts/setup_fckeditor.js
@@ -1,21 +1,38 @@
-// hash to store all editor references
-var editors = new Hash();
+applyOrRemoveFCKeditors = function() {
+  filter = $$('select.columnsFilter').first();
+
+  // transform all textareas to FCKeditors, but only if filter is set to plain HTML
+  if(filter && $F(filter) == '') {
+    // by default, apply FCKeditor to all textareas
+    $$('textarea').each(function(t) {
+      // some calculations
+      height = t.getDimensions()['height'];
+      if(height == 0) height = 200; // default height = 200px
+
+      // initialize FCKeditor
+      FCKeditor.BasePath = '/javascripts/adva_fckeditor/fckeditor/';
+      var oFCKeditor = new FCKeditor(t.id, '100%', height, 'adva-cms');
+      oFCKeditor.Config['CustomConfigurationsPath'] = '/javascripts/adva_fckeditor/config.js';
+      oFCKeditor.ReplaceTextarea();
+    });
+  } else {
+    // otherwise remove instances
+    $$('textarea').each(function(t) {
+      f = $(t.id + '___Frame');
+      c = $(t.id + '___Config');
+      if(f) f.remove();
+      if(c) c.remove();
+      $(t).show();
+    });
+  }
+}
 
 Event.onReady(function() {
-  // by default, apply FCKeditor to all textareas
-  $$('textarea').each(function(t) {
-    // some calculations
-    height = t.getDimensions()['height'];
-    if(height == 0) height = 200; // default height = 200px
+  applyOrRemoveFCKeditors();
+});
 
-    // initialize FCKeditor
-    var oFCKeditor = new FCKeditor(t.id);
-    oFCKeditor.BasePath = '/javascripts/adva_fckeditor/fckeditor/';
-    oFCKeditor.Config['CustomConfigurationsPath'] = '/javascripts/adva_fckeditor/config.js';
-    oFCKeditor.ToolbarSet = 'adva-cms';
-    oFCKeditor.Height = height;
-    oFCKeditor.ReplaceTextarea();
-    // store all references
-    editors[t.id] = oFCKeditor;
-  });
+Event.addBehavior({
+  'select.columnsFilter:change':function(e) {
+    applyOrRemoveFCKeditors();
+  }
 });
