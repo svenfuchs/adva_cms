3662e4e012eac692360ebf50dc860a3dd0cf4430
Sven Fuchs <svenfuchs@artweb-design.de>
2009-01-28 19:24:47 +0100

    convert adva_asset integration tests/stories

--- engines/adva_assets/test_/test_helper.rb
+++ engines/adva_assets/test_/test_helper.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../adva_cms/test_/test_helper')
 
-Dir[File.dirname(__FILE__) + "/test_init/**/*.rb"].each { |path| require path }
+Dir[File.dirname(__FILE__) + "/test_helper/**/*.rb"].each { |path| require path }
 
-Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) +  '/test_init/fixtures'
+Test::Unit::TestCase.fixture_path = File.dirname(__FILE__) +  '/test_helper/fixtures'
