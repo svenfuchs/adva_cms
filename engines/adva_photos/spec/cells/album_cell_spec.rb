6725152c0eab76bf6e790154f30ad1711c1bc066
Clemens Kofler <clemens@railway.at>
2009-01-29 16:13:03 +0100

    Fix broken specs.

--- engines/adva_photos/spec/cells/album_cell_spec.rb
+++ engines/adva_photos/spec/cells/album_cell_spec.rb
@@ -2,7 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe AlbumCell do
   it "renders" do
-    controller = mock('controller', :perform_caching => false)
+    site = mock('site', :id => 1)
+    section = mock('section', :id => 1, :track_method_calls => nil) # TODO: use real object?
+    controller = mock('controller', :perform_caching => false, :site => site, :section => section)
     cell = AlbumCell.new(controller, nil)
     cell.render_state(:single).should =~ /album/i
   end
--- engines/adva_user/spec/cells/user_cell_spec.rb
+++ engines/adva_user/spec/cells/user_cell_spec.rb
@@ -2,7 +2,9 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe UserCell do
   it "renders" do
-    controller = mock('controller', :perform_caching => false)
+    site = mock('site', :id => 1)
+    section = mock('section', :id => 1, :track_method_calls => nil) # TODO: use real object?
+    controller = mock('controller', :perform_caching => false, :site => site, :section => section)
     cell = UserCell.new(controller, nil)
     cell.render_state(:recent).should =~ /recent users/i
   end
