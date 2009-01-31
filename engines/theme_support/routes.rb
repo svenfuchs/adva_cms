11fe9ec8c054bda34e45f1f5e4859a2f473af549
Sven Fuchs <svenfuchs@artweb-design.de>
2009-01-31 21:02:56 +0100

    split populate.rb into smaller files and move them to [engine]/test/test_helper/database.rb, also move engine specific contexts to [engine]/test/test_helper/contexts.rb

--- engines/theme_support/routes.rb
+++
@@ -1,9 +0,0 @@
-# map.connect ':type/themes/:theme_id/*file',
-#              :controller => 'theme',
-#              :action => 'file',
-#              :requirements => { :type => /stylesheets|javascripts|images/ }
-
-map.connect 'themes/:theme_id/:type/*file',
-             :controller => 'theme',
-             :action => 'file',
-             :requirements => { :type => /stylesheets|javascripts|images/ }
