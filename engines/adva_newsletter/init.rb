198d4bc1d46e34ae38dc6dfad117405c403f0a22
Clemens Kofler <clemens@railway.at>
2009-02-07 19:56:47 +0100

    Removed a few unused files.

--- engines/adva_newsletter/init.rb
+++ engines/adva_newsletter/init.rb
@@ -1,10 +1,3 @@
 # remove plugin from load_once_paths 
 ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{|path| path =~ %r(^#{File.dirname(__FILE__)}) }
 I18n.load_path += Dir[File.dirname(__FILE__) + '/locale/**/*.yml']
-
-# add JavaScripts
-# for Rails 2.3
-# ActionView::Helpers::AssetTagHelper.javascript_expansions[:adva_cms_admin] += ['adva_cms/admin/newsletter.js']
-
-# for Rails 2.2
-ActionView::Helpers::AssetTagHelper::JavaScriptSources.expansions[:adva_cms_admin] += ['adva_cms/admin/newsletter.js']
