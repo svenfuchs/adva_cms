ab2265b6687fc66f1830bdc926da7cee0e15ad76
Sven Fuchs <svenfuchs@artweb-design.de>
2009-02-09 09:05:20 +0100

    apparently on rails 2.3 transactions are already started and rolled back?

--- engines/adva_cms/test/test_helper/setup_db_transaction.rb
+++ engines/adva_cms/test/test_helper/setup_db_transaction.rb
@@ -1,13 +1,15 @@
+# apparently on rails 2.3 transactions are already started and rolled back?
+
 class Test::Unit::TestCase
   def start_db_transaction!
-    ActiveRecord::Base.connection.increment_open_transactions
-    ActiveRecord::Base.connection.begin_db_transaction
+    # ActiveRecord::Base.connection.increment_open_transactions
+    # ActiveRecord::Base.connection.begin_db_transaction
   rescue SQLite3::SQLException => e
   end
 
   def rollback_db_transaction!
-    ActiveRecord::Base.connection.rollback_db_transaction
-    ActiveRecord::Base.connection.decrement_open_transactions
+    # ActiveRecord::Base.connection.rollback_db_transaction
+    # ActiveRecord::Base.connection.decrement_open_transactions
   rescue SQLite3::SQLException => e
   end
 end
