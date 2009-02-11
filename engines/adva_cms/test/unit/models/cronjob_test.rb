141601009b4eefb9001db682ff492066f8fc0bd1
priit.mx.ee <priit@mx.ee>
2009-02-11 17:45:17 +0200

    adva_cms: cronjob_test passing

--- engines/adva_cms/test/unit/models/cronjob_test.rb
+++ engines/adva_cms/test/unit/models/cronjob_test.rb
@@ -1,140 +1,112 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../test_helper')
 
-# FIXME something needs to be stubbed here. this seems to have spawed hundreds of
-# Ruby processes bringing my machine almost completely to halt.
-#
-# It leaves a bunch of cronjobs in my crontab ... which curiously doesn't even seem
-# to be saved to a file?
-#
-# Ok, figured it out. The second test class contained a teardown cleanup hook which
-# I had commented out because I wanted to focus on the first class. Well, this still
-# seems dangerous to me. What if that hook never gets called for some reason.
-#
-# There's a DummyCrontab class in cronedit, maybe one could use that?
-
+#TODO: There's a DummyCrontab class in cronedit, maybe one could use that?
 class CronjobTest < ActiveSupport::TestCase
   def setup
     super
-    # FIXME move to database/populate
-    Cronjob.delete_all
     @cronjob = Cronjob.create! :command => "test_command"
   end
   
-  teardown do
+  def teardown
+    super
     remove_all_test_cronjobs
   end
 
-  test "a cronjob with a command is valid" do
+  test "associations" do
+    @cronjob.should belong_to(:cronable, :as => :polymorphic)
+  end
+
+  test "validations" do
     @cronjob.should be_valid
+    @cronjob.should validate_presence_of(:command)
+    @cronjob.should validate_uniqueness_of(:cron_id)
+    @cronjob.should_not validate_presence_of(:due_at)
   end
-
-  test "a cronjob is valid without a due_at date" do
-    @cronjob.due_at = nil
-    @cronjob.should be_valid
+  
+  test "default values" do
+    cronjob = Cronjob.new
+    cronjob.command.should == nil
+    cronjob.due_at.should == nil
+    cronjob.minute.should == "*"
+    cronjob.hour.should == "*"
+    cronjob.day.should == "*"
+    cronjob.month.should == "*"
   end
-
-  test "a cronjob must have a command" do
-    @cronjob.command = nil
-    @cronjob.should_not be_valid
-  end
-
-  test "should have unique cron_id" do
-    #TODO
-  end
-
-  # full_id
-  test "full_id provides 'test-' prefix when in test mode" do
+  
+  test "#full_id should return 'test-' prefix when in test mode" do
     @cronjob.full_id.should == "test-#{RAILS_ROOT}--#{@cronjob.id}"
   end
 
-  test "full_id includes cron_id into full_id" do
+  test "#full_id should return cron_id included" do
     @cronjob.cron_id = "email-deliver-all"
     @cronjob.full_id.should == "test-#{RAILS_ROOT}-email-deliver-all-#{@cronjob.id}"
   end
 
-  # FIXME 
-  # expects:
-  #   export GEM_PATH=/Users/sven/.gem/ruby/1.8:/usr/local/lib/ruby/gems/1.8; /usr/local/bin/ruby -rubygems /Users/sven/Development/projects/adva-cms/adva-cms/script/runner -e test 'test_command; '
-  # but is:
-  #   export GEM_PATH=; /usr/local/bin/ruby -rubygems /Users/sven/Development/projects/adva-cms/adva-cms/script/runner -e test 'test_command; '
-  #
-  # runner_command
-  # test "runner_command returns full runner command with gem path, ruby, command and WITHOUT autoclean" do
-  #   @cronjob.runner_command.should ==
-  #     "export GEM_PATH=#{ENV['GEMDIR']}; " +
-  #     "#{ruby_path} -rubygems #{RAILS_ROOT}/script/runner -e test 'test_command; '"
-  # end
-  # 
-  # test "runner_command returns full runner command with gem path, ruby, command and WITH autoclean" do
-  #   @cronjob.due_at = DateTime.now
-  #   @cronjob.runner_command.should ==
-  #     "export GEM_PATH=#{ENV['GEMDIR']}; " +
-  #     "#{ruby_path} -rubygems #{RAILS_ROOT}/script/runner -e test 'test_command; " +
-  #     "Cronjob.find(#{@cronjob.id}).destroy;'"
-  # end
+  test "#runner_command should return full runner command WITHOUT autoclean)" do
+    @cronjob.runner_command.should ==
+      "export GEM_PATH=#{Gem.path.join(":")}; " +
+      "#{ruby_path} -rubygems #{RAILS_ROOT}/script/runner -e test 'test_command; '"
+  end
 
-  # FIXME does not pass
-  # due_at=
-  # test "due_at= accepts datetime hash and update cronjob fields" do
-  #   @cronjob.due_at = {:minute => "01", :hour => "01", :day => "01", :month => "01"}
-  #   @cronjob.minute.should == "01"
-  #   @cronjob.hour.should == "01"
-  #   @cronjob.day.should == "01"
-  #   @cronjob.month.should == "01"
-  # end
-  # 
-  # test "due_at= accepts DateTime object and update cronjob fields" do
-  #   @cronjob.due_at = DateTime.new 2009,01,15,10,30
-  #   @cronjob.minute.should == "30"
-  #   @cronjob.hour.should == "10"
-  #   @cronjob.day.should == "15"
-  #   @cronjob.month.should == "1"
-  # end
+  test "#runner_command should return full runner command WITH autoclean" do
+    @cronjob.due_at = DateTime.now
+    @cronjob.runner_command.should ==
+      "export GEM_PATH=#{Gem.path.join(":")}; " +
+      "#{ruby_path} -rubygems #{RAILS_ROOT}/script/runner -e test 'test_command; " +
+      "Cronjob.find(#{@cronjob.id}).destroy;'"
+  end
 
-  # due_at
-  test "due_at is nil when there is no exact due-time" do
+  test "#due_at should return DateTime" do
+    @cronjob.due_at = Time.now
+    @cronjob.due_at.class.should == DateTime
+  end
+
+  test "#due_at should return nil when cronjob does not have one due time" do
     @cronjob.due_at.should be_nil
   end
-  
-  # FIXME does not pass
-  # test "due_at is a DateTime" do
-  #   @cronjob.due_at = {:minute => "01", :hour => "01", :day => "01", :month => "01"}
-  #   @cronjob.due_at.should == DateTime.new(Date.today.year, 1, 1, 1, 1)
-  # end
-  # 
-  # test "due_at is nil when there is multiple times" do
-  #   @cronjob.due_at = {:minute => "10/5", :hour => "01", :day => "01", :month => "01"}
-  #   @cronjob.due_at.should be_nil
-  #   @cronjob.due_at = {:minute => "5-10", :hour => "01", :day => "01", :month => "01"}
-  #   @cronjob.due_at.should be_nil
-  # end
-  
-  # FIXME regex does not match:
-  # "##__test-/Users/sven/Development/projects/adva-cms/adva-cms--136__\n*\t*\t*\t*\t*\texport GEM_PATH=; /usr/local/bin/ruby -rubygems /Users/sven/Development/projects/adva-cms/adva-cms/script/runner -e test 'test_command; '\n" =~ 
-  # /\#\#__test\-\/Users\/sven\/Development\/projects\/adva\-cms\/adva\-cms\-\-__\n\*\t\*\t\*\t\*\t\*\texport\ GEM_PATH/. /\#\#__test\-\/Users\/sven\/Development\/projects\/adva\-cms\/adva\-cms\-\-__\n\*\t\*\t\*\t\*\t\*\texport\ GEM_PATH/
-  #
-  # # create
-  # 
-  # test "create should create CronEdit cronjob" do
-  #   cronjob = Cronjob.new :command => "test_command"
-  #   cronjob.save
-  #   jobs = `crontab -l`
-  #   jobs.should =~ cronjob_regexp(cronjob)
-  # end
-  # 
-  # # destroy
-  # 
-  # test "destroy should remove CronEdit cronjob" do
-  #   cronjob = Cronjob.new :command => "test_command"
-  #   cronjob.save
-  #   cronjob.destroy
-  #   jobs = `crontab -l`
-  #   (jobs =~ cronjob_regexp(cronjob)).should be_nil
-  # end
+
+  test "#due_at should return nil when there is multiple times" do
+    @cronjob.due_at = Time.now
+    @cronjob.due_at.should_not be_nil
+    @cronjob.minute = "10/5"
+    @cronjob.due_at.should be_nil
+    @cronjob.minute = "5-10"
+    @cronjob.due_at.should be_nil
+  end
+
+  test "#due_at= should accept DateTime object and update cronjob fields" do
+    @cronjob.due_at = DateTime.new 2009,01,15,10,30
+    @cronjob.minute.should == "30"
+    @cronjob.hour.should == "10"
+    @cronjob.day.should == "15"
+    @cronjob.month.should == "1"
+  end
+
+  test "#due_at= should convert TimeWithZone to localtime so cronjob will be at same timezone as OS" do
+    Time.zone = 2 # OS time zone
+    time_in_user_time_zone = Time.zone.local(2009,01,15, 10,30,0).in_time_zone(8)
+
+    @cronjob.due_at = time_in_user_time_zone   
+    @cronjob.minute.should == "30"
+    @cronjob.hour.should == "10" # should be in OS time zone
+    @cronjob.day.should == "15"
+    @cronjob.month.should == "1"
+  end
+
+  test "#create should create CronEdit cronjob" do
+    Cronjob.destroy_all
+    cronjob = Cronjob.create! :command => "test_command"
+    `crontab -l`.should =~ cronjob_regexp(cronjob.id)
+  end
+
+  test "#destroy should remove CronEdit cronjob" do
+    @cronjob.destroy
+    `crontab -l`.should_not =~ cronjob_regexp(@cronjob.id)
+  end
 end
 
-def cronjob_regexp(model)
-  /#{ Regexp.escape("##__test-#{RAILS_ROOT}--#{model.id}__\n*\t*\t*\t*\t*\texport GEM_PATH") }/
+def cronjob_regexp(model_id)
+  /#{ Regexp.escape("##__test-#{RAILS_ROOT}--#{model_id}__\n*\t*\t*\t*\t*\t") }/
 end
 
 def ruby_path
