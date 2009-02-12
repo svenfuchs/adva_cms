733c59b15a573e5e8dbac6b299ac6e47fa924750
Thomas R. Koll <tomk32@tomk32.de>
2009-02-12 12:54:27 +0100

    added missing brackets in Calendar#days_in_month_with_events fixed and re-added calendar unit tests

--- engines/adva_calendar/test/unit/models/calendar_test.rb
+++ engines/adva_calendar/test/unit/models/calendar_test.rb
@@ -1,39 +1,41 @@
-# require File.dirname(__FILE__) + '/../../test_helper'
-#
-# class CalendarTest < ActiveSupport::TestCase
-#   def setup
-#     super
-#     @calendar = Calendar.find_by_permalink!('calendar-with-events')
-#     @calendar.events.update_all(:published_at => Time.now)
-#     @calendar_without_events = Calendar.find_by_permalink!('calendar-without-events')
-#   end
-#
-#   test "01 is a kind of Section" do
-#     @calendar.should be_kind_of(Section)
-#   end
-#
-#   test "02 content_type returns 'CalendarEvent'" do
-#     Calendar.content_type.should == 'CalendarEvent'
-#   end
-#
-#   test "03 days_in_month_with_events for month with no events" do
-#     @calendar.days_in_month_with_events(Date.civil(1999,12)).should be_empty
-#   end
-#
-#   test "04 days_in_month_with_events with events" do
-#     @calendar.instance_variable_set('@days_in_month_with_events', {})
-# #    @calendar.days_in_month_with_events(Date.civil(2009,2,1)).should ==[Date.civil(2009,2,1), Date.civil(2009,2,2), Date.civil(2009,2,1), Date.civil(2009,2,8)].flatten.compact.sort
-#     @calendar.days_in_month_with_events(Date.civil(2009,1,1)).should ==[Date.civil(2009,1,29), Date.civil(2009,1,30), Date.civil(2009,1,31)]
-#   end
-#
-#   test "05 days_in_month_with_events with less events" do
-#     @calendar.events.update_all(:published_at => nil)
-#     @calendar.instance_variable_set(:@days_in_month_with_events, {})
-# #    @calendar.days_in_month_with_events(Date.civil(2009,2,1)).should == []
-#
-#     # and now publish the second one
-#     @calendar.events.second.update_attribute(:published_at, Time.now)
-#     @calendar.instance_variable_set(:@days_in_month_with_events, {})
-# #    @calendar.days_in_month_with_events(Date.civil(2009,2,1)).should == [Date.civil(2009, 2, 3)]
-#   end
-# end
+require File.dirname(__FILE__) + '/../../test_helper'
+
+class CalendarTest < ActiveSupport::TestCase
+  def setup
+    super
+    @calendar = Calendar.find_by_permalink!('calendar-with-events')
+    @calendar.events.update_all(:published_at => Time.now)
+    @calendar_without_events = Calendar.find_by_permalink!('calendar-without-events')
+  end
+
+  test "01 is a kind of Section" do
+    @calendar.should be_kind_of(Section)
+  end
+
+  test "02 content_type returns 'CalendarEvent'" do
+    Calendar.content_type.should == 'CalendarEvent'
+  end
+
+  test "03 days_in_month_with_events for month with no events" do
+    @calendar.days_in_month_with_events(Date.civil(1999,12)).should be_empty
+  end
+
+  test "04 days_in_month_with_events with events" do
+    @calendar.instance_variable_set('@days_in_month_with_events', {})
+    @calendar.days_in_month_with_events(Date.civil(2009,2,1)).should ==[Date.civil(2009,2,1), Date.civil(2009,2,2), Date.civil(2009,2,3), Date.civil(2009,2,8)].flatten.compact.sort
+    @calendar.days_in_month_with_events(Date.civil(2009,1,1)).should ==[Date.civil(2009,1,29), Date.civil(2009,1,30), Date.civil(2009,1,31)]
+  end
+
+  test "05 days_in_month_with_events with less events" do
+    @calendar.events.update_all(:published_at => nil)
+    @calendar.instance_variable_set(:@days_in_month_with_events, nil)
+    @calendar.days_in_month_with_events(Date.civil(2009,1,1)).should == []
+    @calendar.days_in_month_with_events(Date.civil(2009,2,1)).should == []
+
+    # and now publish the second one
+    @calendar.events.find_by_permalink('a-past-event').update_attribute(:published_at, Time.now)
+    @calendar.instance_variable_set(:@days_in_month_with_events, {})
+    @calendar.days_in_month_with_events(Date.civil(2009,1)).should == [Date.civil(2009, 1, 31)]
+    @calendar.days_in_month_with_events(Date.civil(2009,2)).should == []
+  end
+end
