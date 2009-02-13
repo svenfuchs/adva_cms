9e2ed68485c5596afedc82bce4ddaf8036be43bf
Sven Fuchs <svenfuchs@artweb-design.de>
2009-02-13 15:16:13 +0100

    add Object#in?

--- engines/adva_cms/lib/core_ext/object_try.rb
+++ engines/adva_cms/lib/core_ext/object_try.rb
@@ -1,7 +1,11 @@
-# thx chris wanstrath, http://ozmm.org/posts/try.html
 class Object
+  # thx chris wanstrath, http://ozmm.org/posts/try.html
   def try(method, *args)
     send method, *args if respond_to? method
+  end
+  
+  def in?(*array)
+    array.include?(self)
   end
 end
 
