018f6cc0538a1512c47cf9b16cf20fa59b5d4e63
Sven Fuchs <svenfuchs@artweb-design.de>
2009-02-13 13:28:21 +0100

    force_html routing filter: do not append extension for urls starting with admin/

--- plugins/routing-filter/lib/routing_filter/force_html.rb
+++ plugins/routing-filter/lib/routing_filter/force_html.rb
@@ -4,14 +4,14 @@ module RoutingFilter
   class ForceHtml < Base
     def around_recognize(path, env, &block)
       # remove the extension from the path
-      path.gsub! /\.html$/, ''
+      path.gsub! /\.html$/, '' unless path =~ %r(^/admin)
       yield(path, env)
     end
 
     def around_generate(*args, &block)
       returning yield do |result|
         # append the extension to the path unless it has a known extension
-        unless result.blank? or result == '/'
+        unless result =~ %r(^/admin) or result.blank? or result == '/'
           extensions = Mime::EXTENSION_LOOKUP.keys
           result.replace(result.sub(/(\?|$)/, '.html\1')) unless result =~ /\.#{extensions.join('|')}(\?|$)/
         end
