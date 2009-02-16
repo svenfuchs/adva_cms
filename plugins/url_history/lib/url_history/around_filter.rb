ed68d8a0215475b690648d244112e803df40fe92
Sven Fuchs <svenfuchs@artweb-design.de>
2009-02-16 16:14:23 +0100

    url_history: use send for accessing protected :current_resource method on controllers

--- plugins/url_history/lib/url_history/around_filter.rb
+++ plugins/url_history/lib/url_history/around_filter.rb
@@ -6,15 +6,19 @@ module UrlHistory
         url = controller.request.url.gsub(/\?.*$/, '')
         Entry.find(:first, :conditions => ["url = ? ", url]) or create_entry!(controller, url)
       end
-      
+
       protected
-      
+
         def create_entry!(controller, url)
-          if !url.match(%r(/admin/)) and resource = controller.try(:current_resource)
-            Entry.create! :url => url, :resource => resource, :params => stringify(controller.params)
+          if !url.match(%r(/admin/)) and resource = current_resource(controller)
+            Entry.create!(:url => url, :resource => resource, :params => stringify(controller.params))
           end
         end
-        
+
+        def current_resource(controller)
+          controller.send(:current_resource) rescue nil
+        end
+
         def stringify(hash)
           hash.each { |key, value| hash[key] = value.to_s if value.is_a?(Symbol) }
           hash
