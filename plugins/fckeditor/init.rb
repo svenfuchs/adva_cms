b32dde82f79ea70d8ef1ccba269b86ac835e1133
Clemens Kofler <clemens@railway.at>
2009-02-16 12:21:56 +0100

    Enable possibility to opt-in for FCKeditor.

--- plugins/fckeditor/init.rb
+++ plugins/fckeditor/init.rb
@@ -1,9 +1,2 @@
 # remove plugin from load_once_paths 
 ActiveSupport::Dependencies.load_once_paths -= ActiveSupport::Dependencies.load_once_paths.select{ |path| path =~ %r(^#{File.dirname(__FILE__)}) }
-
-# include FCKeditor
-# for Rails 2.3
-# ActionView::Helpers::AssetTagHelper.javascript_expansions[:adva_cms_admin] += ['fckeditor/fckeditor/fckeditor.js', 'fckeditor/setup_fckeditor.js']
-
-# for Rails 2.2
-# ActionView::Helpers::AssetTagHelper::JavaScriptSources.expansions[:adva_cms_admin] += ['fckeditor/fckeditor/fckeditor.js', 'fckeditor/setup_fckeditor.js']
