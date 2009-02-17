f022f1085d64d3956185c2f96815779db8d1b66f
Clemens Kofler <clemens@railway.at>
2009-02-17 17:24:31 +0100

    Fix validations for subscription model.

--- engines/adva_cms/app/models/subscription.rb
+++ engines/adva_cms/app/models/subscription.rb
@@ -2,7 +2,7 @@ class Subscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :subscribable, :polymorphic => true, :counter_cache => true
 
-  validates_presence_of :user_id
+  validates_presence_of :user_id, :if => lambda { |subscription| subscription.user.nil? }
   validates_uniqueness_of :user_id, :scope => [:subscribable_id, :subscribable_type]
 
   named_scope :confirmed, :conditions => "confirmed_at IS NOT NULL"
