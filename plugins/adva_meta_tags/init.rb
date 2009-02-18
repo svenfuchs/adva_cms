b966991bbc5a33ea3ae8c1c90e19ea65f780259e
Sven Fuchs <svenfuchs@artweb-design.de>
2009-02-18 13:01:46 +0100

    doh. gotta require 'application' in rails 2.2.x.
    tests passed locally, maybe because i still had an application_controller file from my work on the 2.3 branch?

--- plugins/adva_meta_tags/init.rb
+++ plugins/adva_meta_tags/init.rb
@@ -2,6 +2,8 @@ require 'meta_tags/article_form_builder'
 # require 'meta_tags/helper'
 
 config.to_prepare do
+  require 'application'
+  
   BaseController.helper :meta_tags
   Admin::BaseController.helper :meta_tags
 
