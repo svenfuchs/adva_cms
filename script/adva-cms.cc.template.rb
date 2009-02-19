2b7d7096afb90fdc0c2e9dfa4619a480a01445b2
Marko Seppae <marko.seppa@gmail.com>
2009-02-19 10:27:52 +0100

    template for cruise control

---
+++ script/adva-cms.cc.template.rb
@@ -0,0 +1,41 @@
+def patch_file(path, after, insert)
+  content = File.open(path) { |f| f.read }
+  content.gsub!(after, "#{after}\n#{insert}") unless content =~ /#{Regexp.escape(insert)}/mi
+  File.open(path, 'w') { |f| f.write(content) }
+end
+
+run "rm -rf /tmp/adva_cms > /dev/null; cd /tmp"
+run "rails -q adva_cms; cd /tmp/adva_cms"
+
+File.unlink 'public/index.html' rescue Errno::ENOENT
+
+rakefile("adva-cms.rake") do
+  <<-src
+    require 'tasks/rails'
+    load 'vendor/adva/engines/adva_cms/lib/tasks/adva_cms.rake'
+  src
+end
+
+file 'script/test-adva-cms', <<-src
+  #!/usr/bin/env ruby
+  paths = ARGV.clone
+  load 'vendor/adva/script/test'
+src
+
+patch_file 'config/environment.rb',
+  "require File.join(File.dirname(__FILE__), 'boot')",
+  "require File.join(File.dirname(__FILE__), '../vendor/adva/engines/adva_cms/boot')"
+  
+run "cp -r /srv/cruise/projects/adva-cms/work /tmp/adva_cms/vendor/adva"
+
+rake "rails:freeze:gems"
+run "patch -p0 < vendor/adva/patch-2.3/rails-2.3.patch"
+
+rake "assets:copy"
+rake "db:migrate:prepare"
+run  "rake db:migrate > /dev/null"
+rake "db:test:clone"
+rake "db:schema:dump"
+
+run "cd /tmp/adva_cms"
+run "vendor/adva/script/test vendor/adva/engines/ -p"
