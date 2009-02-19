a4e00c0b5efdfda2fff30821509fc33ca33c14a5
Marko Seppae <marko.seppa@gmail.com>
2009-02-19 15:47:15 +0100

    fixed cc template

--- script/adva-cms.cc.template.rb
+++ script/adva-cms.cc.template.rb
@@ -1,6 +1,6 @@
 TEST_DIR        = '/tmp/adva_cms'
-TEST_ADVA_DIR   = TEST_DIR + '/vendor/adva'
 CC_DIR          = '/srv/cruise/projects/adva-cms/work'
+RAILS_DIR       = '/srv/cruise/shared/rails'
 start_time      = Time.now # Just for fun :)
 
 def patch_file(path, after, insert)
@@ -28,14 +28,16 @@ patch_file 'config/environment.rb',
   "require File.join(File.dirname(__FILE__), 'boot')",
   "require File.join(File.dirname(__FILE__), '../vendor/adva/engines/adva_cms/boot')"
 
-run "cp -r #{CC_DIR} #{TEST_ADVA_DIR}"
+run "ln -s #{CC_DIR} #{TEST_DIR}/vendor/adva"
+run "ln -s #{RAILS_DIR} #{TEST_DIR}/vendor/rails"
 
-rake  "rails:freeze:gems"
-run   "patch -p0 < vendor/adva/patch-2.3/rails-2.3.patch"
+# rake  "rails:freeze:gems"
+# run   "patch -p0 < vendor/adva/patch-2.3/rails-2.3.patch"
 
-rake  "db:migrate:prepare"
-run   "rake db:migrate"
-rake  "db:test:clone"
+rake "db:migrate:prepare"
+rake "db:migrate"
+rake "db:test:clone"
+rake "assets:copy"
 
 end_time = (Time.now - start_time).to_i
 puts  "Rails app setup, time elapsed #{end_time} seconds."
