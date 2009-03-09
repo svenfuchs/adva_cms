d61bc8eb3182a8c2bac0fb828d4d911402634a8f
Sven Fuchs <svenfuchs@artweb-design.de>
2009-03-09 10:16:05 +0100

    add a nested plugin locator and move engine dependend plugins to their engines' vendor/plugins directories (calendar_date_select, later_dude, authorization, viking)

--- engines/adva_cms/boot.rb
+++ engines/adva_cms/boot.rb
@@ -17,7 +17,13 @@ Rails::Configuration.class_eval do
   end
   
   def default_plugin_loader
-    Rails::Plugin::PublishingLoader
+    Rails::Plugin::RegisteringLoader
+  end
+  
+  def default_plugin_locators
+    locators = []
+    locators << Rails::Plugin::GemLocator if defined? Gem
+    locators << Rails::Plugin::NestedFileSystemLocator
   end
   
   def default_plugins
@@ -43,7 +49,7 @@ module Rails
   end
 
   class Plugin
-    class PublishingLoader < Rails::Plugin::Loader # ummm, what's a better name?
+    class RegisteringLoader < Rails::Plugin::Loader # ummm, what's a better name?
       def register_plugin_as_loaded(plugin)
         Rails.plugins[plugin.name.to_sym] = plugin
         super
@@ -60,6 +66,16 @@ module Rails
   
     def register_stylesheet_expansion(*args)
       ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion *args
+    end
+    
+    class NestedFileSystemLocator < FileSystemLocator
+      def locate_plugins_under(base_path)
+        plugins = super
+        Dir["{#{plugins.map(&:directory).join(',')}}/vendor/plugins"].each do |path|
+          plugins += super(path)
+        end
+        plugins
+      end
     end
   end
 end
