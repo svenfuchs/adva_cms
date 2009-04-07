842817f6fd7e85972b6c5c8e95a3facee800c16e
Luca Guidi <guidi.luca@gmail.com>
2009-04-07 17:42:03 +0200

    Configuration for Webrat + Selenium

--- engines/adva_cms/test/test_helper/setup_webrat.rb
+++ engines/adva_cms/test/test_helper/setup_webrat.rb
@@ -2,7 +2,8 @@ require 'webrat'
 require 'webrat/rails'
 
 Webrat.configure do |config|
-  config.mode = :rails
+  config.application_environment = :test # needed by webrat < 0.4.4
+  config.mode = ENV["WEBRAT_MODE"] == "selenium" ? :selenium : :rails
   config.open_error_files = false
 end
 
