b6405a63b30e51aae92eca5b35672058763d8880
Luca Guidi <guidi.luca@gmail.com>
2009-04-14 16:09:19 +0200

    Started migration of adva_assets to jQuery

--- engines/adva_assets/public/javascripts/adva_assets/admin/asset.js
+++ engines/adva_assets/public/javascripts/adva_assets/admin/asset.js
@@ -1,3 +1,42 @@
+(function($) {
+  $.fn.addInput = function(options) {
+    return this.each(function() {
+      var list = $("#files div"), copyFrom = list.find("p:first"), tagall = $("#tagall_files");
+      var newNode = copyFrom.clone(true), files = list.children("p");
+      newNode.find("input").each(function(){ $(this).val("") });
+      var close = newNode.find(".remove_file:first").show();
+      newNode.find(".tagall_files:first").remove();
+      close.click(function(){
+        $(this).parent("p").remove();
+        if(tagall.is(":visible") && files.length == 1) tagall.hide();
+      });
+      if(!tagall.is(":visible") && files.length > 0) tagall.show();
+      newNode.appendTo(list);
+    });
+  };
+  $.fn.applyTagsToAll = function(options) {
+    return this.each(function() {
+      // TODO DRY/clean-up map and trim tags stuff
+      var tags = $(this).siblings(".taglist_field").val().split(" ");
+      tags = jQuery.map(tags, function(tag, i){ return jQuery.trim(tag.toString()); });
+      $("#files div p input.taglist_field").each(function(){
+        var localTags = $(this).val().split(" ");
+        localTags = jQuery.map(localTags, function(tag, i){ return jQuery.trim(tag.toString()); });
+        jQuery.each(tags, function(){
+          if(jQuery.inArray(this.toString(), localTags) < 0)
+            localTags.push(this.toString());
+        });
+        $(this).val(jQuery.trim(localTags.join(" ")));
+      });
+    });
+  };
+})(jQuery);
+
+$().ready(function(){
+  $("#asset_add_file").click(function(){ $(this).addInput(); });
+  $("#tagall_files").click(function(){ $(this).applyTagsToAll(); });
+});
+
 Asset = {
   addInput: function() {
     var list = $('files'), copyFrom = list.down(), tagall = $('tagall_files');
@@ -17,7 +56,7 @@ Asset = {
     if(!tagall.visible() && files.length > 0) tagall.show();
     list.appendChild(newNode);
   },
-  
+
   removeInput: function(input, formId, inputClass) {
     var length = $$('#' + formId + ' .' + inputClass).findAll(function(e) { return e.visible(); }).length;
     if(length == 2) this.showTitle('asset_title');
@@ -52,27 +91,27 @@ Asset = {
   )}
 }
 
-Event.addBehavior({
-  '#asset_add_file:click':  function() { return Asset.addInput(); },
-  '#tagall_files:click':    function() { Asset.applyTagsToAll('asset_form'); },
-  '#assets_search_form':    function() { window.spotlight = new Spotlight('assets_search_form', 'assets_search_query'); },  
-	'.assets_row div:mouseover': 			    function() { Element.getElementsBySelector(this, '.asset_tools').first().show(); },
-	'.assets_row div:mouseout':  			    function() { Element.getElementsBySelector(this, '.asset_tools').first().hide(); }
-});
-function init_asset_filter() {
-	Event.addBehavior({
-	  '#asset-add-file:click':     function() { return Asset.addInput(); },
-	  '#tagall-files:click':       function() { Asset.applyTagsToAll('asset_form'); },
-	  '#assets-search-form':       function() { window.spotlight = new Spotlight('assets-search-form', 'assets-search-query'); },  
-	});
-}
-
-function init_asset_list() {
-	Event.addBehavior({
-	  '.assets-row div:mouseover': function() { Element.getElementsBySelector(this, '.asset-tools').first().show(); },
-	  '.assets-row div:mouseout':  function() { Element.getElementsBySelector(this, '.asset-tools').first().hide(); }
-	});
-}
-
-init_asset_filter();
-init_asset_list();
+// Event.addBehavior({
+//   '#asset_add_file:click':  function() { return Asset.addInput(); },
+//   '#tagall_files:click':    function() { Asset.applyTagsToAll('asset_form'); },
+//   '#assets_search_form':    function() { window.spotlight = new Spotlight('assets_search_form', 'assets_search_query'); },  
+//  '.assets_row div:mouseover':          function() { Element.getElementsBySelector(this, '.asset_tools').first().show(); },
+//  '.assets_row div:mouseout':           function() { Element.getElementsBySelector(this, '.asset_tools').first().hide(); }
+// });
+// function init_asset_filter() {
+//  Event.addBehavior({
+//    '#asset-add-file:click':     function() { return Asset.addInput(); },
+//    '#tagall-files:click':       function() { Asset.applyTagsToAll('asset_form'); },
+//    '#assets-search-form':       function() { window.spotlight = new Spotlight('assets-search-form', 'assets-search-query'); },  
+//  });
+// }
+// 
+// function init_asset_list() {
+//  Event.addBehavior({
+//    '.assets-row div:mouseover': function() { Element.getElementsBySelector(this, '.asset-tools').first().show(); },
+//    '.assets-row div:mouseout':  function() { Element.getElementsBySelector(this, '.asset-tools').first().hide(); }
+//  });
+// }
+// 
+// init_asset_filter();
+// init_asset_list();
