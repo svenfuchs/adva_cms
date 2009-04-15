6fa5e1757892aeb911094c94a119d05bee9af992
Clemens Kofler <clemens@railway.at>
2009-04-15 13:43:07 +0200

    Fix duplicate results when filtering.

--- engines/adva_cms/vendor/plugins/has_filter/lib/has_filter/active_record/act_macro.rb
+++ engines/adva_cms/vendor/plugins/has_filter/lib/has_filter/active_record/act_macro.rb
@@ -2,7 +2,7 @@
 # http://www.pathf.com/blogs/2008/06/more-named-scope-awesomeness/
 
 module HasFilter
-  # # raised when filter_by is called with an attribute that has not been 
+  # # raised when filter_by is called with an attribute that has not been
   # # whitelisted for being filtered
   # class IllegalAttributeAccessError < ActiveRecordError
   #   attr_reader :attribute
@@ -18,7 +18,7 @@ module HasFilter
         return if has_filter?
         include InstanceMethods
         extend ClassMethods
-        
+
         class_inheritable_accessor :filter_chain
         self.filter_chain = ::HasFilter::Filter::Chain.build(self, filters)
 
@@ -38,11 +38,11 @@ module HasFilter
         scopes.each do |name, scope|
           named_scope name, lambda { |column, value| filter_scope(column, Array(value), *scope) }
         end
-        
+
         named_scope :contains_all, contains_all_scope
         named_scope :categorized, categorized_scope
       end
-      
+
       def filtered(filters)
         scope = scope(:find).blank? ? self : scoped(scope(:find))
         filter_chain.select(filters).scope(scope)
@@ -52,7 +52,7 @@ module HasFilter
         included_modules.include? HasFilter::ActiveRecord::InstanceMethods
       end
     end
-  
+
     module ClassMethods
       protected
         def contains_all_scope
@@ -62,23 +62,23 @@ module HasFilter
             { :conditions => [(["#{column} LIKE ?"] * values.size).join(' AND '), *values] }
           }
         end
-        
+
         def categorized_scope
-          lambda { |*ids| 
+          lambda { |*ids|
             { :select => "#{table_name}.*, COUNT(*) AS count",
-              :joins => { :categorizations => :category }, 
+              :joins => { :categorizations => :category },
               :conditions => ["#{reflect_on_association(:categories).table_name}.id IN(?)", ids],
               :group => "#{table_name}.id HAVING count >= #{ids.size}" }
           }
         end
-      
+
         def filter_scope(column, values, operator, format = nil)
           query = (["#{column} #{operator} ?"] * values.size)
           values = values.map{ |value| format ? format % value : value }.map(&:downcase)
           scope = { :conditions => [query.join(' OR '), *values] }
-          translated?(column) ? scope.merge!(:joins => :globalize_translations, :conditions => ['current = ?', true]) : scope
+          translated?(column) ? scope.merge!(:joins => :globalize_translations, :group => "#{self.table_name}.id", :conditions => ['current = ?', true]) : scope
         end
-        
+
         def translated?(column)
           # FIXME really should be in globalize
           respond_to?(:globalize_options) && globalize_options[:translated_attributes].include?(column.to_sym)
