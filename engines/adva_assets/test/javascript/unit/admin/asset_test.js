e7a66d5ee1b2b6cb19c41e79aa864215f406d53b
Luca Guidi <guidi.luca@gmail.com>
2009-04-15 12:32:53 +0200

    Initial Javascript tests for adva_assets

---
+++ engines/adva_assets/test/javascript/unit/admin/asset_test.js
@@ -0,0 +1,85 @@
+// TODO inject TestUtils methods in window
+var TestUtils = {
+  defaultTags: "adva tags",
+
+  files: function() {
+    return $("#files div").children("p");
+  },
+  lastFileRow: function(){
+    return $("#files div").children("p:last");
+  },
+  simulateClick: function() {
+    jQuery("#asset_add_file").click();
+  },
+  copyFrom: function() {
+    return $("#files div").find("p:first");
+  },
+  tagAll: function() {
+    jQuery("#tagall_files").click();
+  },
+  fillCopyFromWithTags: function(element, tags) {
+    element = element || this.copyFrom();
+    tags = tags || this.defaultTags;
+    element.find("input:last").val(tags);
+  },
+  emptyCopyFromTags: function() {
+    this.fillCopyFromWithTags("");
+  }
+}
+
+module("addInput");
+test("should add input row on click", function() {
+  equals(TestUtils.files().size(), 1);
+  TestUtils.simulateClick();
+  equals(TestUtils.files().size(), 2, "it should have two input rows after click");
+});
+
+test("should empty inputs when new created", function() {
+  TestUtils.fillCopyFromWithTags();
+  TestUtils.simulateClick();
+  TestUtils.lastFileRow().find("input").each(function(){
+    equals($(this).val(), "");
+  });
+});
+
+test("should hide 'remove_file' link for first row", function(){
+  ok(!TestUtils.copyFrom().find(".remove_file:first").is(":visible"), "'remove_file' shouldn't be visible");
+});
+
+test("should show 'remove_file' link for new rows", function(){
+  TestUtils.simulateClick();
+  ok(TestUtils.lastFileRow().find(".remove_file:first").is(":visible"), "'remove_file' should be visible");
+});
+
+test("should show 'tagall_files' link for first row", function(){
+  ok(TestUtils.copyFrom().find(".tagall_files:first").is(":visible"), "'tagall_files' should be visible");
+});
+
+test("should remove 'tagall_files' link for new rows", function(){
+  TestUtils.simulateClick();
+  // TODO replace with proper QUnit existence assertion
+  equals(TestUtils.lastFileRow().find(".tagall_files:first").size(), 0, "'tagall_files' should be removed");
+});
+
+test("should remove input row when click on 'remove_file' link", function(){
+  TestUtils.simulateClick();
+  var filesCount = TestUtils.files().size();
+  TestUtils.lastFileRow().find(".remove_file:first").click();
+  equals(TestUtils.files().size(), filesCount - 1, "should remove row when click on 'remove_file' link");
+});
+
+module("applyTagsToAll");
+test("should apply tags to all when click on 'tagall_files'", function(){
+  TestUtils.simulateClick();
+  TestUtils.tagAll();
+  TestUtils.files().find("input:last").each(function(){
+    equals($(this).val(), TestUtils.defaultTags);
+  });
+});
+
+test("should add tags when click on 'tagall_files' and input has existing tags", function(){
+  TestUtils.simulateClick();
+  TestUtils.fillCopyFromWithTags(TestUtils.lastFileRow(), "rails");
+  TestUtils.tagAll();
+  equals(TestUtils.lastFileRow().find("input:last").val(), "rails " + TestUtils.defaultTags);
+});
