91a1de9a23b076cd8dcf844fc15f971c2a8cfeb1
Clemens Kofler <clemens@railway.at>
2009-04-16 10:58:49 +0200

    Ported flash to jQuery.

---
+++ engines/adva_cms/public/javascripts/adva_cms/jquery.flash.js
@@ -0,0 +1,36 @@
+var Flash = {
+	transferFromCookies: function() {
+	  var data = JSON.parse(unescape(Cookie.get('flash')).replace(/\+/g, ' '));
+	  if(!data) data = {};
+	  Flash.data = data;
+	  Cookie.erase('flash');
+	},
+	show: function(type, message) {
+	  if(!Flash.data || Flash.data == {}) Flash.transferFromCookies();
+
+    var flash = $('#flash_' + type);
+    // if no message is given, look it up in the hash
+    if(!message) var message = Flash.data[type];
+
+    if(!message) return;
+    
+    if(message.toString().match(/<li/)) message = "<ul>" + message + '</ul>'
+    flash.html(message);
+
+		flash.show();
+	},
+	
+	error: function(message) {
+    this.show('error', message);
+  },
+
+  notice: function(message) {
+    this.show('notice', message);
+  }
+}
+
+$(document).ready(function() {
+  $.each(['notice', 'error'], function() {
+    Flash.show(this.toString());
+  })
+})
