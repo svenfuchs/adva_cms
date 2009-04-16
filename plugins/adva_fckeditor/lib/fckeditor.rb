aa4ccc14c0d148cb49f40b2d3a0131cb4ec966f2
Clemens Kofler <clemens@railway.at>
2009-04-16 12:33:48 +0200

    adva_fckeditor javascripts should sit in the right directory.

--- plugins/adva_fckeditor/lib/fckeditor.rb
+++ plugins/adva_fckeditor/lib/fckeditor.rb
@@ -1,6 +1,6 @@
 class Fckeditor
   def self.load!
     # load FCKeditor
-    ActionView::Helpers::AssetTagHelper.register_javascript_expansion :admin  => ['fckeditor/fckeditor/fckeditor.js', 'fckeditor/setup_fckeditor.js']
+    ActionView::Helpers::AssetTagHelper.register_javascript_expansion :admin  => ['adva_fckeditor/fckeditor/fckeditor.js', 'adva_fckeditor/setup_fckeditor.js']
   end
 end
