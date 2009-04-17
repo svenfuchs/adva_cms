39bc74c1c42aa79a33c9c363e847047879497194
Clemens Kofler <clemens@railway.at>
2009-04-17 11:50:56 +0200

    Don't include old JS files.

--- engines/adva_comments/init.rb
+++ engines/adva_comments/init.rb
@@ -6,6 +6,6 @@ ActiveSupport::Dependencies.autoloaded_constants -= %w(ActionController::ActsAsC
 
 require 'format'
 
-register_javascript_expansion :admin   => %w( adva_comments/admin/comments )
+register_javascript_expansion :admin   => %w()
 register_stylesheet_expansion :default => %w( adva_comments/comments ),
                               :admin   => %w( adva_comments/admin/comments )
