967369a37ba21b0d931f1457ae4a72d1c72afb46
Clemens Kofler <clemens@railway.at>
2009-04-17 17:09:36 +0200

    A little more refactoring.

--- engines/adva_cms/public/javascripts/adva_cms/admin/jquery.article.js
+++ engines/adva_cms/public/javascripts/adva_cms/admin/jquery.article.js
@@ -1,17 +1,22 @@
-$(document).ready(function() {
-  $('a#add_excerpt').click(function(event) {
+var Excerpt = {
+  show: function(event) {
     event.preventDefault();
     $('#article_excerpt_wrapper').show();
     $('#add_excerpt_hint').hide();
     $('#hide_excerpt_hint').show();
     $('#article_excerpt').removeAttr('disabled');
-  });
+  },
 
-  $('a#hide_excerpt').click(function(event) {
+  hide: function(event) {
     event.preventDefault();
     $('#article_excerpt_wrapper').hide();
     $('#hide_excerpt_hint').hide();
     $('#add_excerpt_hint').show();
     $('#article_excerpt').attr('disabled', 'disabled');
-  });
+  }
+}
+
+$(document).ready(function() {
+  $('a#add_excerpt').click(Excerpt.show);
+  $('a#hide_excerpt').click(Excerpt.hide);
 });
