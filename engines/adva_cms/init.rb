7f8ffaa214e670ea0f7f93cce674cc851a8d1659
Clemens Kofler <clemens@railway.at>
2009-04-17 14:37:34 +0200

    Ported cached pages to jQuery.

--- engines/adva_cms/init.rb
+++ engines/adva_cms/init.rb
@@ -40,7 +40,7 @@ register_javascript_expansion \
   :login => %w(),
   :default => %w( adva_cms/jquery.roles adva_cms/jquery.dates adva_cms/parseuri adva_cms/application ),
   :simple => %w(),
-  :admin => %w( adva_cms/admin/jquery.article )
+  :admin => %w( adva_cms/admin/jquery.article adva_cms/admin/jquery.cached_pages )
 
 register_stylesheet_expansion \
   :default => %w( adva_cms/default adva_cms/common adva_cms/forms ),
