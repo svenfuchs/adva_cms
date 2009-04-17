f53582ffe02d7cdc274b5418694a52cf289a2af2
Clemens Kofler <clemens@railway.at>
2009-04-17 12:44:27 +0200

    Ported filter to jQuery.

--- engines/adva_cms/vendor/plugins/has_filter/lib/has_filter.rb
+++ engines/adva_cms/vendor/plugins/has_filter/lib/has_filter.rb
@@ -5,9 +5,8 @@ module HasFilter
     form_tag(options.delete(:url) || request.path, :method => :get, :id => 'filters', :class => 'filters') do
       klass.filter_chain.to_form_fields(self, options).join("\n") + "\n" +
       content_tag(:div, :class => 'submit') do
-        link_to_function(I18n.t(:"filter.submit.value", :default => 'Apply'), '$("filters").submit()')
+        content_tag(:button, I18n.t(:'filter.submit.value', :default => 'Apply'))
       end
-      # submit_tag(I18n.t(:"filter.submit.value", :default => 'Apply'), :class => 'submit')
     end
   end
 end
