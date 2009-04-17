5206b6374615e8f69edaab690fd8204074eeec3b
Clemens Kofler <clemens@railway.at>
2009-04-17 16:11:29 +0200

    Finalized articles conversion to jQuery.

--- engines/adva_cms/public/javascripts/adva_cms/admin/jquery.article.js
+++ engines/adva_cms/public/javascripts/adva_cms/admin/jquery.article.js
@@ -5,5 +5,21 @@ $(document).ready(function() {
     } else {
       $('#article_published_at_wrapper').show();
     }
-  })
+  });
+
+  $('a#add_excerpt').click(function(event) {
+    event.preventDefault();
+    $('#article_excerpt_wrapper').show();
+    $('#add_excerpt_hint').hide();
+    $('#hide_excerpt_hint').show();
+    $('#article_excerpt').removeAttr('disabled');
+  });
+
+  $('a#hide_excerpt').click(function(event) {
+    event.preventDefault();
+    $('#article_excerpt_wrapper').hide();
+    $('#hide_excerpt_hint').hide();
+    $('#add_excerpt_hint').show();
+    $('#article_excerpt').attr('disabled', 'disabled');
+  });
 });
