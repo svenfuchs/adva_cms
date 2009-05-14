09503928271439c060e580613d058dfbc0d3dfa5
Clemens Kofler <clemens@railway.at>
2009-05-14 11:58:25 +0200

    Moved cells output filter where it belongs.

--- plugins/adva_cells/test/test_helper.rb
+++ plugins/adva_cells/test/test_helper.rb
@@ -1,10 +1,10 @@
-require File.expand_path(File.dirname(__FILE__) + '/../../../engines/adva_cms/test/test_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../../adva_cms/test/test_helper')
 
 class CellTestController
   def site
     @site ||= Site.first
   end
-  
+
   def section
     site.sections.first
   end
