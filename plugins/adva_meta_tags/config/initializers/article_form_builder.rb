c0c3b50855073bd2935f9765470c5530412a84e7
Clemens Kofler <clemens@railway.at>
2009-05-18 17:14:05 +0200

    Include hint for meta geourl.

--- plugins/adva_meta_tags/config/initializers/article_form_builder.rb
+++ plugins/adva_meta_tags/config/initializers/article_form_builder.rb
@@ -5,7 +5,7 @@ class ArticleFormBuilder < ExtensibleFormBuilder
         <fieldset class="clearfix">
           <div class="col">
             #{ f.text_field(:meta_author, :label => true) }
-            #{ f.text_field(:meta_geourl, :label => true) }
+            #{ f.text_field(:meta_geourl, :label => true, :hint => I18n.t(:'adva.meta_tags.hints.meta_geourl', :meta_geourl_link => %(<a href="#{I18n.t(:'adva.meta_tags.hints.meta_geourl_link')}" target="_blank">#{I18n.t(:'adva.meta_tags.hints.meta_geourl_link')}</a>))) }
             #{ f.text_field(:meta_copyright, :label => true) }
           </div>
   
--- plugins/adva_meta_tags/init.rb
+++ plugins/adva_meta_tags/init.rb
@@ -1,3 +1,5 @@
+I18n.load_path += Dir[File.dirname(__FILE__) + '/config/locales/*.{yml,rb}']
+
 config.to_prepare do
   BaseController.helper :meta_tags
   Admin::BaseController.helper :meta_tags
