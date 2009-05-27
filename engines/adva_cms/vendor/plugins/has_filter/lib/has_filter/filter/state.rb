db8034f7e476f41fcf947220cabf55ba3e80c8dd
Clemens Kofler <clemens@railway.at>
2009-05-27 13:56:16 +0200

    German filter translations.

--- engines/adva_cms/vendor/plugins/has_filter/lib/has_filter/filter/state.rb
+++ engines/adva_cms/vendor/plugins/has_filter/lib/has_filter/filter/state.rb
@@ -7,18 +7,18 @@ module HasFilter
   		def to_form_fields(options = {})
   			@options[:states].map { |state| state_tag(state) }
   		end
-		
+
   		def scope(target)
 	      # FIXME assert that state is a valid state!
   		  selected.inject(target) { |target, state| target.send(state) }
   		end
-		
+
   		protected
-  		
+
   		  def state_tag(state)
   		    id = form_field_id(:state, state)
-  		    view.check_box_tag(form_field_name(:state, nil), state, selected.include?(state), :id => id) + 
-  		    view.label_tag(id, state)
+  		    view.check_box_tag(form_field_name(:state, nil), state, selected.include?(state), :id => id) +
+  		    view.label_tag(id, I18n.t(state, :scope => :'has_filter.states', :default => state.to_s))
 		    end
   	end
 	end
