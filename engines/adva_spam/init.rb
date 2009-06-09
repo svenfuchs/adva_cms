6ed4824e6936314a4f5097cca1a56e044c131321
Marko Seppae <marko.seppa@gmail.com>
2009-06-09 11:45:37 +0200

    resolved the class loading issue with adva_spam

--- engines/adva_spam/init.rb
+++ engines/adva_spam/init.rb
@@ -1,3 +1,5 @@
+require File.expand_path(File.dirname(__FILE__) + '/lib/spam_engine/filter')
+
 SpamEngine::Filter.register_default SpamEngine::Filter::Default
 SpamEngine::Filter.register_default SpamEngine::Filter::Akismet
 SpamEngine::Filter.register_default SpamEngine::Filter::Defensio
