70149de3ea89b334f60a0d54aeaac04ce1cc9ca1
Luca Guidi <guidi.luca@gmail.com>
2009-06-17 12:49:52 +0200

    Fixed image paths in mocked controller

--- engines/adva_assets/test/javascript/assets/bucket_add_controller.rb
+++ engines/adva_assets/test/javascript/assets/bucket_add_controller.rb
@@ -2,7 +2,7 @@ class BucketAddController < WEBrick::HTTPServlet::AbstractServlet
   def do_POST(request, response)
     javascript = <<-END
 $('#flash_notice').html('rails.png assigned to this bucket.').show();
-$('#bucket_assets').append('<li><a href="/assets/rails.png" target="_blank"><img="/assets/rails.png" /></a></li>');
+$('#bucket_assets').append('<li><a href="/adva_assets/assets/rails.png" target="_blank"><img="/adva_assets/assets/rails.thumb.png" /></a></li>');
 END
 
     response['Content-Type'] = "text/javascript"
