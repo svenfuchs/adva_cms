efa58a425806f204574069c5dc2cdcb5d68d3ae3
Clemens Kofler <clemens@railway.at>
2009-06-19 15:20:45 +0200

    Injecting behavior in controllers doesn't work as expected -> workaround for adva_newsletter.

--- engines/adva_newsletter/config/initializers/user.rb
+++ engines/adva_newsletter/config/initializers/user.rb
@@ -8,40 +8,39 @@ module Adva
   module Extensions
     module Newsletter
       module User
-        include_into :User
-  
+        include_into 'User'
+
         def self.included(target)
           target.class_eval do
             has_many :subscriptions, :dependent => :destroy, :class_name => "Adva::Subscription"
-            accepts_nested_attributes_for :subscriptions, :reject_if => lambda {|attributes| attributes['subscribe'] != "1" }
+            accepts_nested_attributes_for :subscriptions, :reject_if => lambda { |attributes| attributes['subscribe'] != "1" }
           end
         end
       end
     end
-    
-    module UserController
-      include_into :UserController
 
-      def self.included(target)
-        target.class_eval do
-          before_filter :set_newsletter_attributes
-          include InstanceMethods
-        end
-      end
-
-      module InstanceMethods
-        protected
-          def set_newsletter_attributes
-            @newsletter_attributes = []
-            newsletters = @site.newsletters.published
-            newsletters.each do |newsletter|
-              attributes = {}
-              attributes["subscribable_id"]   = newsletter.id
-              attributes["subscribable_type"] = newsletter.class.to_s
-              @newsletter_attributes << attributes
-            end
-          end
-      end
-    end
+    # FIXME: clashes with Rails' own use of self.inherited (see ActionController::Helpers)
+    # module UserController
+    #   include_into 'UserController'
+    #
+    #   def self.included(target)
+    #     target.class_eval do
+    #       before_filter :set_newsletter_attributes
+    #       include InstanceMethods
+    #     end
+    #   end
+    #
+    #   module InstanceMethods
+    #     protected
+    #     def set_newsletter_attributes
+    #       @newsletter_attributes = @site.newsletters.published.inject([]) do |newsletter_attributes, newsletter|
+    #         newsletter_attributes << returning({}) do |attributes|
+    #           attributes[:subscribable_id]   = newsletter.id
+    #           attributes[:subscribable_type] = newsletter.class.to_s
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
   end
 end
