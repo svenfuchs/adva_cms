2914fc5a8e1cf8ea74e50659582bedb32c5318da
priit.mx.ee <priit@mx.ee>
2009-07-14 15:51:29 +0300

    adva_newsletter: js refactor

--- engines/adva_newsletter/public/javascripts/adva_newsletter/admin/newsletter.js
+++ engines/adva_newsletter/public/javascripts/adva_newsletter/admin/newsletter.js
@@ -9,32 +9,35 @@ AdvaExpandSave = $.klass({
   }
 });
 
-AdvaExpandBodyPlain = $.klass({
+AdvaControlBodyPlain = $.klass({
   initialize: function(target, autofill_src) {
     this.target = $(target);
     this.target_label = $("label[for='" + this.target.attr("id") + "']");
     this.autofill_src = $(autofill_src);
   },
   onclick: function(event) {
+    this.target.is(":hidden") ? this.show() : this.hide()
+    event.preventDefault();
+  },
+  show: function() {
+    this.autofill();
     this.toggle_controls();
     this.toggle_target();
-
-    event.preventDefault();
+  },
+  hide: function() {
+    if (confirm($("#adva_newsletter_issue_confirm_remove_body_plain").val())) {
+      this.target.val("");
+      this.toggle_controls();
+      this.toggle_target();
+    }
   },
   toggle_controls: function() {
     this.element.siblings().show();
     this.element.hide();
   },
   toggle_target: function() {
-    this.autofill();
-    this.target_label.toggle();
-    if (this.target.is(":hidden")) {
-      this.target.show();
-      this.target.attr("disabled", false);
-    } else {
-      this.target.hide();
-      this.target.attr("disabled", true);
-    }
+      this.target_label.toggle();
+      this.target.toggle();
   },
   autofill: function() {
     if (this.target.is(":hidden") && (this.target.val().length === 0)) {
@@ -42,24 +45,13 @@ AdvaExpandBodyPlain = $.klass({
     }
   },
   autofill_value: function() {
-    alert(this.fckeditor_present());
     var text = "";
-    if (this.fckeditor_present()) {
-      text = $(this.fckeditor_instance().GetData()).text();
-      alert(text);
-    } else {
-      text = this.autofill_src.val();
-      alert(text);
-    }
-    return text
+    text = (this.fckeditor_present()) ? $(this.fckeditor_instance().GetData()).text() : this.autofill_src.val()
+    return text;
   },
   fckeditor_present: function() {
     if (typeof FCKeditorAPI != "undefined") {
-      if (typeof this.fckeditor_instance() != "undefined") {
-        return true;
-      } else {
-        return false;
-      }
+      return (typeof(this.fckeditor_instance()) != "undefined")
     } else {
       return false;
     }
@@ -72,5 +64,5 @@ AdvaExpandBodyPlain = $.klass({
 
 jQuery(function($) {
   $("#send_later").attach(AdvaExpandSave, "#send_later_expanded");
-  $(".issue_body_plain_controls").attach(AdvaExpandBodyPlain, "#issue_body_plain", "#issue_body");
+  $(".issue_body_plain_controls").attach(AdvaControlBodyPlain, "#issue_body_plain", "#issue_body");
 });
