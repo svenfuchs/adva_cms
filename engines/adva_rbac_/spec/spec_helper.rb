b6e7947003cbde457e983e5644cfa5ee20a1fabf
Sven Fuchs <svenfuchs@artweb-design.de>
2009-07-27 14:02:47 +0200

    remove old adva_rbac implementation

--- engines/adva_rbac_/spec/spec_helper.rb
+++
@@ -1,3 +0,0 @@
-require File.dirname(__FILE__) + '/../../adva_cms/spec/spec_helper'
-
-require File.expand_path(File.dirname(__FILE__) + '/spec_roles_helper')
--- engines/adva_rbac_/spec/spec_local_helper.rb
+++
@@ -1,8 +0,0 @@
-# does not include environment.rb
-
-unless defined?(RAILS_ROOT) # environment.rb included?
-  require File.expand_path(File.dirname(__FILE__) + '/mock_rails')
-  require File.expand_path(File.dirname(__FILE__) + '/mock_models')
-end
-
-require File.expand_path(File.dirname(__FILE__) + '/spec_roles_helper')
