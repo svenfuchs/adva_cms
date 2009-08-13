f9d8a9942373d4f6997400925346000bd994f5e1
Clemens Kofler and Sven Fuchs <dev+clemens+svenfuchs@adva-cms.org>
2009-08-13 15:24:05 +0200

    fix emails to not use legacy system_email method anymore

--- engines/adva_user/app/models/user_mailer.rb
+++ engines/adva_user/app/models/user_mailer.rb
@@ -1,22 +1,24 @@
 class UserMailer < ActionMailer::Base
-  include Login::MailConfig
-
   class << self
     def handle_user_registered!(event)
-      deliver_signup_verification_email event.object, verification_url(event.source, event.object)
+      deliver_signup_verification_email(
+        :user => event.object, 
+        :verification_url => verification_url(event.source, event.object), 
+        :from => site(event.source.site).email_from
+      )
     end
     
     protected
     
       def verification_url(controller, user)
-        controller.send :url_with_token, user, 'verification', :action => 'verify'
+        controller.send(:url_with_token, user, 'verification', :action => 'verify')
       end
   end
 
-  def signup_verification_email(user, verification_url)
-    recipients user.email
-    from system_email(verification_url)
-    subject I18n.t(:'adva.signup.notifications.email_verification.subject')
-    body :user => user, :verification_url => verification_url
+  def signup_verification_email(attributes = {})
+    recipients attributes[:user].email
+    from       attributes[:from]
+    subject    I18n.t(:'adva.signup.notifications.email_verification.subject')
+    body       attributes
   end
 end
