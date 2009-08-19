78b96eeb5fbb13abd68fc6f13d47a7e69573ae14
Clemens Kofler <clemens@railway.at>
2009-08-19 11:18:29 +0200

    Join projection stylesheets to reduce number of files (and load time in dev mode).

--- engines/adva_cms/init.rb
+++ engines/adva_cms/init.rb
@@ -54,8 +54,4 @@ register_stylesheet_expansion \
                   adva_cms/admin/header adva_cms/admin/top adva_cms/admin/sidebar adva_cms/admin/forms 
                   adva_cms/admin/lists adva_cms/admin/content adva_cms/admin/themes adva_cms/admin/helptip
                   adva_cms/admin/users adva_cms/jquery/jquery-ui adva_cms/jquery/jquery.tooltip ),
-  :admin_projection => %w( adva_cms/admin/projection/reset adva_cms/admin/projection/header 
-                           adva_cms/admin/projection/layout adva_cms/admin/projection/sidebar 
-                           adva_cms/admin/projection/top adva_cms/admin/projection/helptip 
-                           adva_cms/admin/projection/common adva_cms/admin/projection/themes
-                           adva_cms/admin/projection/forms )
+  :admin_projection => %w( adva_cms/admin/projection )
