0877fd863304824e98f99a4cea72f257603a99b1
Marko Seppae <marko.seppa@gmail.com>
2009-08-20 11:51:30 +0200

    mouse over tooltip for published and pending icons for contents

--- engines/adva_cms/public/javascripts/adva_cms/admin/jquery.admin.js
+++ engines/adva_cms/public/javascripts/adva_cms/admin/jquery.admin.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
     selected = '#tab_' + $(this).attr('href').replace('#', '');
     $(selected).addClass('active');
   });
+
+  $('.published').hover(
+	function() {
+	  $(this).prepend($("<span title='published' class='status_title'>&nbsp;</span>"));
+    },
+	function() {
+	  $(this).find($("span.status_title").remove());
+    }
+  );
+
+  $('.pending').hover(
+	function() {
+	  $(this).prepend($("<span title='pending' class='status_title'>&nbsp;</span>"));
+    },
+	function() {
+	  $(this).find($("span.status_title").remove());
+    }
+  );
 });
--- engines/adva_cms/public/javascripts/adva_cms/jquery.common.js
+++ engines/adva_cms/public/javascripts/adva_cms/jquery.common.js
@@ -10,7 +10,7 @@ var Form = {
 
 $(document).ready(function() {
   $('#toggle_draft').click(Form.toggleDraft);
-  
+
   if($(".hint").size() > 0) {
     $(".hint").each(function() {
       if(!$(this).hasClass('text_only')) {
