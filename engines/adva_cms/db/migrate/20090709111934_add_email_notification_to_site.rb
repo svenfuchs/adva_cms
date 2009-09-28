f7f814ea80c05e44c8baf6378e0bdb5d741f9bbc
Marko Seppae <marko.seppa@gmail.com>
2009-09-28 11:59:01 +0200

    fixing db file to play nicely with postgresql

--- engines/adva_cms/db/migrate/20090709111934_add_email_notification_to_site.rb
+++ engines/adva_cms/db/migrate/20090709111934_add_email_notification_to_site.rb
@@ -3,7 +3,7 @@ class AddEmailNotificationToSite < ActiveRecord::Migration
   # Throwaway - short term solution
   
   def self.up
-    add_column :sites, :email_notification, :boolean, :default => 0
+    add_column :sites, :email_notification, :boolean, :default => false
   end
   
   def self.down
