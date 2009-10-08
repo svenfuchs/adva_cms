66f95d6131f2c8774ab8cc85bfb4035f0c2e0233
Clemens Kofler <clemens@railway.at>
2009-10-08 17:22:24 +0200

    Fix ActivityNotifierTest to run correctly with Rails 2.3.4.

--- engines/adva_activity/test/unit/activities_notifier_test.rb
+++ engines/adva_activity/test/unit/activities_notifier_test.rb
@@ -12,11 +12,16 @@ class ActivitiesNotifierTest < ActiveSupport::TestCase
     @mail.set_content_type('text', 'plain', { 'charset' => 'utf-8' })
     @mail.mime_version = '1.0'
 
+    # need to stub out Time.now and set mail date in order to avoid automatic setting
+    # see http://github.com/rails/rails/commit/f73d34c131c1e371c76c5a146aac2c2bffbf96e5
+    stub(Time).now { Time.local(2009, 10, 8, 12, 0, 0) }
+    @mail.date = Time.now
+
     @site = Site.first
     @section = Section.first
     @user = User.first
   end
-  
+
   def teardown
     super
     ActionMailer::Base.deliveries.clear
@@ -25,7 +30,7 @@ class ActivitiesNotifierTest < ActiveSupport::TestCase
   test "sets the mail up correctly for articles" do
     article = Article.first
     activity = activity_for(article)
-    
+
     @mail.subject = "[#{@site.name} / #{@section.title}] New Article posted"
     @mail.from    = "#{@site.name} <#{@site.email}>"
     @mail.body    = "#{article.author_name} <#{article.author_email}> just posted a new Article on #{@site.name} in section #{@section.title}."
@@ -37,7 +42,7 @@ class ActivitiesNotifierTest < ActiveSupport::TestCase
   test "sets the mail up correctly for comments" do
     comment = Comment.first
     activity = activity_for(comment)
-    
+
     @mail.subject = "[#{@site.name} / #{@section.title}] New Comment posted"
     @mail.from    = "#{@site.name} <#{@site.email}>"
     @mail.body    = "#{comment.author_name} <#{comment.author_email}> just posted a new Comment on #{@site.name} in section #{@section.title}."
@@ -50,7 +55,7 @@ class ActivitiesNotifierTest < ActiveSupport::TestCase
     test "sets the mail up correctly for wikipages" do
       wikipage = Wikipage.first
       activity = activity_for(wikipage)
-    
+
       @mail.subject = "[#{@site.name} / #{@section.title}] New Wikipage posted"
       @mail.from    = "#{@site.name} <#{@site.email}>"
       @mail.body    = "#{wikipage.author_name} <#{wikipage.author_email}> just posted a new Wikipage on #{@site.name} in section #{@section.title}."
@@ -59,7 +64,7 @@ class ActivitiesNotifierTest < ActiveSupport::TestCase
       ActivityNotifier.create_new_content_notification(activity, @user).encoded.should == @mail.encoded
     end
   end
-  
+
   def activity_for(object)
     returning Activity.new(:site => @site, :section => @section) do |activity|
       activity.object = object
