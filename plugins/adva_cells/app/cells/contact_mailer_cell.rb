f27c0e9595fa2e982b1c56d74b9db4156c422f0c
Clemens Kofler <clemens@railway.at>
2010-01-21 16:59:21 +0100

    remove unused contact mailer code

--- plugins/adva_cells/app/cells/contact_mailer_cell.rb
+++ plugins/adva_cells/app/cells/contact_mailer_cell.rb
@@ -1,11 +1,7 @@
 include Authentication::HashHelper
 
 class ContactMailerCell < BaseCell
-  tracks_cache_references :recent, :track => ['@section', '@articles']
-  
-  has_state :recent
-  
-  helper :content, :resource, :contact_mailer
+  helper :contact_mailer
   
   def mailer_form
     @recipients = URI.escape(EzCrypto::Key.encrypt_with_password(ContactMail.password, send(:site_salt), @opts["recipients"])) if @opts["recipients"]
