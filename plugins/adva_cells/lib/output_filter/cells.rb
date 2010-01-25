e10e4b8dba9a3124dded78579d5bf4fe7e7bc127
Clemens Kofler <clemens@railway.at>
2010-01-25 11:03:47 +0100

    simple translations for cells

--- plugins/adva_cells/lib/output_filter/cells.rb
+++ plugins/adva_cells/lib/output_filter/cells.rb
@@ -4,7 +4,13 @@ module OutputFilter
       def cells(html)
         cells = html.scan(/(<cell[^>]*\/\s*>|<cell[^>]*>.*?<\/cell>)/m).inject({}) do |cells, matches|
           tag = matches.first
-          attrs = Hash.from_xml(tag)['cell']
+
+          # simple i18n ... TODO: write a test
+          translated_tag = tag.gsub(/<translation>\s*([.\w]+)\s*<\/translation>/) do |match|
+            I18n.t($1)
+          end
+
+          attrs = Hash.from_xml(translated_tag)['cell']
           name, state = attrs.delete('name').split('/')
           cells[tag] = [name, state, attrs]
           cells
@@ -29,7 +35,7 @@ module OutputFilter
       if controller.response.body.is_a?(String)
         cells = parser.cells(controller.response.body)
         controller.response.body.gsub!(/(#{cells.keys.join('|')})/) do |tag|
-          controller.response.template.render_cell *cells[tag]
+          controller.response.template.render_cell(*cells[tag])
         end unless cells.empty?
       end
     end
