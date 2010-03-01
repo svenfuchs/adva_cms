d83a932d50689711a98470ab42751e35d047523a
Raphaela Wrede <rwrede@adva-business.com>
2010-03-01 11:51:28 +0100

    SitesController refactoring

--- engines/adva_cms/app/controllers/admin/sites_controller.rb
+++ engines/adva_cms/app/controllers/admin/sites_controller.rb
@@ -1,4 +1,5 @@
 class Admin::SitesController < Admin::BaseController
+
   before_filter :params_site, :only => [:new, :create]
   before_filter :params_section, :only => [:new, :create]
   before_filter :protect_single_site_mode, :only => [:index, :new, :create, :destroy]
@@ -10,35 +11,38 @@ class Admin::SitesController < Admin::BaseController
   helper :activities
 
   def index
-    @sites = Site.paginate(:page => params[:page], :per_page => params[:per_page], :order => 'id')
-  end
-
-  def show
-    @users = @site.users_and_superusers # TODO wanna show only *recent* users here
-    @contents = @site.unapproved_comments.group_by(&:commentable)
-    @activities = @site.activities.find_coinciding_grouped_by_dates(Time.zone.now.to_date, 1.day.ago.to_date)
+    @sites = @account.sites
+    @sites = @sites.paginate(:page => params[:page], :per_page => params[:per_page], :order => 'id')
   end
 
   def new
   end
 
+  def show
+    @users = @site.users_and_superusers
+    @contents = @site.unapproved_comments.group_by(&:commentable)
+    @activities = @site.activities.find_coinciding_grouped_by_dates(Time.zone.now.to_date, 1.day.ago.to_date)
+  end
+
   def create
     site = Site.new params[:site]
+    site.adva_best_account = @account
     section = site.sections.build(params[:section])
     site.sections << section
-
     if site.save
+      @site = site
       flash[:notice] = t(:'adva.sites.flash.create.success')
-      redirect_to admin_site_url(site)
+      redirect_to admin_site_path(@site)
     else
       flash.now[:error] = t(:'adva.sites.flash.create.failure')
-      render :action => :new
+      render :action => :new, :account_id => @account
     end
   end
 
   def edit
   end
 
+  # TODO: Tests missing
   def update
     if @site.update_attributes params[:site]
       flash[:notice] = t(:'adva.sites.flash.update.success')
@@ -49,6 +53,7 @@ class Admin::SitesController < Admin::BaseController
     end
   end
 
+  # TODO: Tests missing
   def destroy
     if @site.destroy
       flash[:notice] = t(:'adva.sites.flash.destroy.success')
@@ -59,21 +64,28 @@ class Admin::SitesController < Admin::BaseController
     end
   end
 
-  private
+  protected
+
+    def set_account
+      # account_id only present for index action
+      @account = AdvaBestAccount.find_by_id(params[:account_id])
+      # all other actions are in the scope of a site and the account can be found through the site
+      @account = Site.find_by_id(params[:id]).adva_best_account unless @account
+    end
 
     def set_menu
       @menu = case params[:action]
-      when 'show'
-        Menus::Admin::Sites.new
       when 'edit'
-        Menus::Admin::Settings.new
+        Menus::Admin::AdvaBestSettings.new
+      when 'new'
+        Menus::Admin::AdvaBestSites.new
       else
-        Menus::Admin::Sites::Main.new
+        Menus::Admin::AdvaBestSites::Main.new
       end
     end
 
     def set_site
-      @site = Site.find params[:id] if params[:id]
+      @site = @account.sites.find(params[:id]) if params[:id] if @account
     end
 
     def params_site
@@ -90,11 +102,15 @@ class Admin::SitesController < Admin::BaseController
       params[:section][:title] ||= Section.types.first
     end
 
+    def current_resource
+      context = super
+      context.new_record? ? @account : context
+    end
+
     def protect_single_site_mode
       unless Site.multi_sites_enabled
         if params[:action] == 'index'
-          site = Site.find_by_host(request.host_with_port)
-          site = Site.first unless site
+          site = Site.find_or_initialize_by_host(request.host_with_port)
           redirect_to admin_site_url(site)
         else
           render :action => :multi_sites_disabled, :layout => 'simple'
