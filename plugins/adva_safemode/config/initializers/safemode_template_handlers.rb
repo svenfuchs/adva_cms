fb24b5df97e8120e59cec0aa16bf63223d593e35
Matthias Viehweger <kronn@kronn.de>
2010-03-17 09:31:19 +0100

    TemplateHandler "serb" registered in safemode-plugin

---
+++ plugins/adva_safemode/config/initializers/safemode_template_handlers.rb
@@ -0,0 +1,3 @@
+ActionView::Template.register_template_handler :serb, ActionView::TemplateHandlers::SafeErb
+# not tested, yet
+# ActionView::Template.register_template_handler :haml, ActionView::TemplateHandlers::SafeHaml
