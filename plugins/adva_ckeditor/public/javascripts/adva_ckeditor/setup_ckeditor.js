77a2e6db31b1052955f84a4ae59f8c238ce73aea
Matthias Viehweger <kronn@kronn.de>
2010-03-22 17:30:09 +0100

    only provide upload through CKEditor if Adva::Assets is installed

--- plugins/adva_ckeditor/public/javascripts/adva_ckeditor/setup_ckeditor.js
+++ plugins/adva_ckeditor/public/javascripts/adva_ckeditor/setup_ckeditor.js
@@ -1,31 +1,35 @@
 applyOrRemoveFCKeditors = function() {
+  var has_assets = $('#index_assets').length > 0;
+
+  if(has_assets) {
+    url_match = /(.*)\/sites\/(\d+)\/(.*)/i.exec(window.location.href);
+    var site_id = url_match[2];
+  }
 
   $('textarea.wysiwyg').each(function() {
-    
+
     $.each(CKEDITOR.instances, function () {
       CKEDITOR.remove(this);
     });
 
     filter = $('select.columnsFilter')[0];
-    
+
     // transform all textareas to FCKeditors, but only if filter is set to plain HTML or no filter is defined
     if(typeof filter == 'undefined' || $(filter).val() == '') {
-      
+
       if($(this).hasClass('small')) {
         CKEDITOR.replace($(this).attr('name'), {
           toolbar : 'Basic'
         });
       } else {
-          url_match = /(.*)\/sites\/(\d+)\/(.*)/i.exec(window.location.href);
-          site_id = url_match[2];
+        if(has_assets) {
           CKEDITOR.replace($(this).attr('name'), {
-            // filebrowserBrowseUrl : '/javascripts/adva_ckeditor/ckfinder/ckfinder.html',
-            // filebrowserImageBrowseUrl : '/javascripts/adva_ckeditor/ckfinder/ckfinder.html?type=Images',
             filebrowserImageUploadUrl : '/admin/sites/' + site_id + '/assets',
             filebrowserImageBrowseUrl : '/admin/sites/' + site_id + '/assets'
-            //filebrowserBrowseUrl : '/admin/sites/' + site_id + '/assets'
-            //filebrowserBrowseUrl : '/admin/sites/' + site_id + '/assets'
           });
+        } else {
+          CKEDITOR.replace($(this).attr('name'));
+        }
       }
     } else {
         cke = $('#cke_' + $(this).attr('id'));
