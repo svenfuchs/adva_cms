eee316bf10bc08d5788b3e343e222ec2b8dca865
Matthias Viehweger <kronn@kronn.de>
2010-04-06 18:01:04 +0200

    apply rails_ext.rb only if Rails is present

--- plugins/adva_safemode/lib/safemode/rails_ext.rb
+++ plugins/adva_safemode/lib/safemode/rails_ext.rb
@@ -1,14 +1,16 @@
-class RequestJail < Safemode::Jail
-  allow :request_uri
-end
-
-module ActionController
-  class TestRequest < Request
-    class Jail < RequestJail
-    end
+if defined?(Rails)
+  class RequestJail < Safemode::Jail
+    allow :request_uri
   end
-  class Request < Rack::Request
-    class Jail < RequestJail
+
+  module ActionController
+    class TestRequest < Request
+      class Jail < RequestJail
+      end
+    end
+    class Request < Rack::Request
+      class Jail < RequestJail
+      end
     end
   end
 end
