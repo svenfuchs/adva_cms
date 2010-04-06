6f5605d5246747a4ee460ca730644f18b9077e29
Matthias Viehweger <kronn@kronn.de>
2010-04-06 14:34:24 +0200

    moved jail-management back into blankslate-metaclass because integration fails atm

--- plugins/adva_safemode/lib/safemode/blankslate.rb
+++ plugins/adva_safemode/lib/safemode/blankslate.rb
@@ -9,6 +9,36 @@ module Safemode
       silently { undef_methods(*instance_methods - @@allow_class_methods) }
 
       def method_added(name) end # ActiveSupport needs this
+
+      def inherited(subclass)
+        subclass.init_allowed_methods(@allowed_methods)
+      end
+
+      def init_allowed_methods(allowed_methods)
+        @allowed_methods = allowed_methods
+      end
+
+      def allowed_methods
+        @allowed_methods ||= []
+      end
+
+      def allow(*names)
+        @allowed_methods = alter_allowed_method_list(names, :+)
+      end
+
+      def disallow(*names)
+        @allowed_methods = alter_allowed_method_list(names, :-)
+      end
+
+      def allowed?(name)
+        allowed_methods.include? name.to_s
+      end
+
+      private
+
+      def alter_allowed_method_list(names, alter_method = :+)
+        am = allowed_methods.send(alter_method, names.map{|name| name.to_s}).uniq
+      end
     end
   end
 end
--- plugins/adva_safemode/lib/safemode/jail.rb
+++ plugins/adva_safemode/lib/safemode/jail.rb
@@ -1,37 +1,5 @@
 module Safemode
   class Jail < Blankslate
-    class << self
-      def inherited(subclass)
-        subclass.init_allowed_methods(@allowed_methods)
-      end
-
-      def init_allowed_methods(allowed_methods)
-        @allowed_methods = allowed_methods
-      end
-
-      def allowed_methods
-        @allowed_methods ||= []
-      end
-
-      def allow(*names)
-        @allowed_methods = alter_allowed_method_list(names, :+)
-      end
-
-      def disallow(*names)
-        @allowed_methods = alter_allowed_method_list(names, :-)
-      end
-
-      def allowed?(name)
-        allowed_methods.include? name.to_s
-      end
-
-      private
-
-      def alter_allowed_method_list(names, alter_method = :+)
-        am = allowed_methods.send(alter_method, names.map{|name| name.to_s}).uniq
-      end
-    end
-
     def initialize(source = nil)
       @source = source
     end
