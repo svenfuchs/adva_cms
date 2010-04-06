a3d09bcf4479940925011409f12c1c4259850418
Matthias Viehweger <kronn@kronn.de>
2010-04-06 10:38:01 +0200

    doc update

--- plugins/adva_safemode/lib/safemode/blankslate.rb
+++ plugins/adva_safemode/lib/safemode/blankslate.rb
@@ -1,7 +1,8 @@
 module Safemode
   class Blankslate
     @@allow_instance_methods = ['class', 'inspect', 'methods', 'respond_to?', 'to_s', 'instance_variable_get']
-    @@allow_class_methods    = ['methods', 'new', 'name', 'inspect', '<', 'ancestors', '=='] # < needed in Rails Object#subclasses_of
+    @@allow_class_methods    = ['methods', 'new', 'name', 'inspect', '<', 'ancestors', '==']
+    # < needed in Rails Object#subclasses_of, ancestors and == is needed by rails/generate
 
     silently { undef_methods(*instance_methods - @@allow_instance_methods) }
     class << self
