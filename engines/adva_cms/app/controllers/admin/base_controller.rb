c81b665099b149ef1e67ce83c9f79ed084dc4edd
Raphaela Wrede <rwrede@adva-business.com>
2010-04-13 10:26:29 +0200

    bugfix: articles are shown in correct language on website after being edited in backend

--- engines/adva_cms/app/controllers/admin/base_controller.rb
+++ engines/adva_cms/app/controllers/admin/base_controller.rb
@@ -13,6 +13,8 @@ class Admin::BaseController < ApplicationController
 
   before_filter :set_menu, :set_site, :set_section, :set_locale, :set_timezone
 
+  after_filter :reset_locale
+
   authentication_required
 
   attr_accessor :site
@@ -94,5 +96,9 @@ class Admin::BaseController < ApplicationController
 
     def content_locale
       ActiveRecord::Base.locale == I18n.default_locale ? nil : ActiveRecord::Base.locale
+    end
+
+    def reset_locale
+      ActiveRecord::Base.locale = nil
     end
 end
