d1b2658bc4e52a76d594acece13c6e6d166840df
Matthias Viehweger <kronn@kronn.de>
2010-04-14 14:45:35 +0200

    added Jail for ActiveSupport::TimeWithZone

--- plugins/adva_safemode/lib/safemode/rails_ext.rb
+++ plugins/adva_safemode/lib/safemode/rails_ext.rb
@@ -13,4 +13,18 @@ if defined?(Rails)
       end
     end
   end
+
+  module ActiveSupport
+    class TimeWithZone
+      class Jail < Safemode::Jail
+        allow %w(blank? duplicable? present?
+              _dump asctime ctime day dst? getgm getlocal getutc gmt?
+              gmt_offset gmtime gmtoff hash hour httpdate isdst iso8601
+              localtime mday min minus_without_duration mon month
+              plus_without_duration rfc2822 rfc822 sec strftime succ to_date
+              to_datetime to_f to_i tv_sec tv_usec usec utc utc? utc_offset
+              wday xmlschema yday year zone to_formatted_s).map(&:to_sym)
+      end
+    end
+  end
 end
